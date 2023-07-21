64d67b5d7e7ba661b87f21bf82340e3443099c55
timbastin <bastin.tim@gmail.com>
2023-07-21 17:11:04 +0200

    Fixed permission error during database migration

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 run::
-	go run main.go
+	go run ./cmd/flawfix/main.go
 
 
 clean::
--- initdb.sql
+++ initdb.sql
@@ -1,4 +1,7 @@
 CREATE DATABASE kratos;
 CREATE USER kratos PASSWORD 'secret';
+GRANT ALL PRIVILEGES ON DATABASE kratos to kratos;
 
-GRANT CONNECT ON DATABASE kratos to kratos;
+\c kratos
+
+GRANT USAGE, CREATE ON SCHEMA public TO kratos;
