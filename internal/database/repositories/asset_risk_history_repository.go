d15594c78d07e9de085a9b4cb20b7c79abbcdd4a
timbastin <bastin.tim@gmail.com>
2024-09-23 19:28:10 +0200

    simplifies text = uuid fix

--- internal/database/repositories/asset_risk_history_repository.go
+++ internal/database/repositories/asset_risk_history_repository.go
@@ -44,27 +44,10 @@ func (r *assetRiskHistoryRepository) GetRiskHistoryByProject(projectId uuid.UUID
 
 	var assetRisk []models.AssetRiskHistory = []models.AssetRiskHistory{}
 
-	//get all asset ids of the project
-	var assetIDs []uuid.UUID
-	if err := r.Repository.GetDB(r.db).Debug().
-		Model(&models.Asset{}).
-		Select("id").
-		Where("project_id = ?", projectId).
-		Find(&assetIDs).Error; err != nil {
-		return nil, err
-	}
-
-	// if no asset ids are found, return empty array
-	if len(assetIDs) == 0 {
-		return assetRisk, nil
-	}
-
 	// get all assetRisk of the project
-	if err := r.Repository.GetDB(r.db).
-		Where("asset_id IN (?)", assetIDs).
-		Where("day = ?", day).
-		Order("day ASC").
-		Find(&assetRisk).Error; err != nil {
+	if err := r.Repository.GetDB(r.db).Where("asset_id IN (?)", r.Repository.GetDB(r.db).Table("assets").Select("id::uuid").Where("project_id = ?", projectId)).Where(
+		"day = ?", day,
+	).Order("day ASC").Find(&assetRisk).Error; err != nil {
 		return nil, err
 	}
 
