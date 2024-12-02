2d2572e83063d421de792650959da2b0ceee7667
Rafi <refaei.shikho@hotmail.com>
2024-12-02 12:41:59 +0100

    fix lint
    Signed-off-by: Rafi <refaei.shikho@hotmail.com>

--- cmd/devguard-scanner/commands/sca.go
+++ cmd/devguard-scanner/commands/sca.go
@@ -73,7 +73,7 @@ func generateSBOM(path string) (*os.File, error) {
 		slog.Info("scanning single file", "file", maybeFilename)
 		// scanning a single file
 		// cdxgenCmd = exec.Command("cdxgen", maybeFilename, "-o", filename)
-		trivyCmd = exec.Command("trivy", "image", "--input", filepath.Base(path), "--format", "cyclonedx", "--output", filename)
+		trivyCmd = exec.Command("trivy", "image", "--input", filepath.Base(path), "--format", "cyclonedx", "--output", filename) // nolint:all // 	There is no security issue right here. This runs on the client. You are free to attack yourself.
 	}
 
 	stderr := &bytes.Buffer{}
