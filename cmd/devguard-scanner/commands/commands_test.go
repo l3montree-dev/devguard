3f215da9be555e5f23cf328b5e3dee84aba602f1
Patrick Rißmann <patrickrissmann@MacBook-Pro-von-Patrick.local>
2025-02-27 17:40:43 +0100

    Added file for testing commands package but no functionality for now

---
+++ cmd/devguard-scanner/commands/commands_test.go
@@ -0,0 +1,30 @@
+// Copyright (C) 2024 Tim Bastin, l3montree UG (haftungsbeschränkt)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+package commands_test
+
+import (
+	"testing"
+)
+
+func TestPrintScaResults(t *testing.T) {
+	t.Run("Return Message", func(t *testing.T) {
+		/*scanResults := scan.ScanResponse{}
+		failOnRisk, assetName, webUI := "", "", ""
+		riskManagement := false*/
+
+	})
+
+}
