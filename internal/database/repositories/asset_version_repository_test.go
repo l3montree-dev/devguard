35125b18c255b11a74057d0c18ea53dc14dfaaaf
Patrick Rißmann <patrickrissmann@MacBook-Pro-von-Patrick.local>
2025-02-27 13:36:14 +0100

    Fixed package naming and paramter type of the assetId

---
+++ internal/database/repositories/asset_version_repository_test.go
@@ -0,0 +1,38 @@
+// Copyright (C) 2024 Tim Bastin, l3montree UG (haftungsbeschränkt)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+package repositories_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/l3montree-dev/devguard/internal/database/repositories"
+)
+
+func TestFindOrCreate(t *testing.T) {
+	t.Run("Returned message", func(t *testing.T) {
+		a := repositories.NewAssetVersionRepository(nil)
+		assetVersionName := "test"
+		b := []byte("497598d2-b90a-4031-b3db-90216de0e17f")
+		assetID, _ := uuid.FromBytes(b)
+		tag := ""
+		defaultBranchName := "main"
+		_, err := a.FindOrCreate(assetVersionName, assetID, tag, defaultBranchName)
+		fmt.Printf("Received the following error : %s", err)
+
+	})
+}
