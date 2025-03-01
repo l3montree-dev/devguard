6ba8561297685638c7d7ad506e1d1b6e488b23d6
Patrick Rißmann <patrickrissmann@Mac.fritz.box>
2025-03-01 12:20:52 +0100

    Now using the database for the test but DatabaseFactory returns error still

--- internal/database/repositories/asset_version_repository_test.go
+++ internal/database/repositories/asset_version_repository_test.go
@@ -15,17 +15,34 @@
 
 package repositories_test
 
-/*func TestFindOrCreate(t *testing.T) {
-	t.Run("Returned message", func(t *testing.T) {
+import (
+	"fmt"
+	"testing"
 
-		a := repositories.NewAssetVersionRepository(nil)
+	"github.com/google/uuid"
+	"github.com/l3montree-dev/devguard/internal/core"
+	"github.com/l3montree-dev/devguard/internal/database/repositories"
+)
+
+func TestFindOrCreate(t *testing.T) {
+	t.Run("Returned message", func(t *testing.T) {
+		core.LoadConfig() // nolint: errcheck
+		core.InitLogger()
+		db, err := core.DatabaseFactory()
+		if err != nil {
+			fmt.Printf("Error when database factory")
+		}
+
+		fmt.Printf("Reached this Code----------------------------------")
+		a := repositories.NewAssetVersionRepository(db)
 		assetVersionName := "test"
 		b := []byte("497598d2-b90a-4031-b3db-90216de0e17f")
 		assetID, _ := uuid.FromBytes(b)
 		tag := ""
 		defaultBranchName := "main"
-		_, err := a.FindOrCreate(assetVersionName, assetID, tag, defaultBranchName)
+
+		_, err = a.FindOrCreate(assetVersionName, assetID, tag, defaultBranchName)
 		fmt.Printf("Received the following error : %s", err)
 
 	})
-}*/
+}
