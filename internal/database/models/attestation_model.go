347b0c69974a0243c89f4f5488cd39e84feedc53
patrick.rissmann@l3montree.com <patrickrissmann@MacBook-Pro-von-Patrick.local>
2025-03-17 14:13:11 +0100

    Added attestation struct to the model and changed asset versions accordingly

--- internal/database/models/attestation_model.go
+++ internal/database/models/attestation_model.go
@@ -1,1 +1,25 @@
 package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type AttestationType string
+
+type Attestation struct {
+	CreatedAt time.Time      `json:"createdAt"`
+	UpdatedAt time.Time      `json:"updatedAt"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
+
+	AttestationName string       `json:"attestationName" gorm:"type:text"`
+	AssetVersionID  string       `json:"assetVersionID" gorm:"primarykey;not null;type:text"`
+	AssetVersion    AssetVersion `json:"assetVersion" gorm:"foreignKey:AssetVersionID;references:Name"`
+
+	//AttestationJSON jsonb `json:"attestationJSON"`
+}
+
+func (m Attestation) TableName() string {
+	return "attestations"
+}
