5eb7e750205d70aeff84f5bac6ba488da567ac00
patrick.rissmann@l3montree.com <patrickrissmann@MacBook-Pro-von-Patrick.local>
2025-03-17 14:24:03 +0100

    Implemented basic level functionality for the attestation repository

--- internal/database/repositories/attestation_repository.go
+++ internal/database/repositories/attestation_repository.go
@@ -1,1 +1,24 @@
 package repositories
+
+import (
+	"github.com/google/uuid"
+	"github.com/l3montree-dev/devguard/internal/common"
+	"github.com/l3montree-dev/devguard/internal/core"
+	"github.com/l3montree-dev/devguard/internal/database/models"
+)
+
+type attestationRepository struct {
+	db core.DB
+	common.Repository[uuid.UUID, models.Attestation, core.DB]
+}
+
+func NewAttestationRepository(db core.DB) *attestationRepository {
+	err := db.AutoMigrate(&models.Attestation{})
+	if err != nil {
+		panic(err)
+	}
+	return &attestationRepository{
+		db:         db,
+		Repository: newGormRepository[uuid.UUID, models.Attestation](db),
+	}
+}
