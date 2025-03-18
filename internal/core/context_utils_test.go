33e7180ff0c8031bffe6880d1d5229c8b9fa5baf
Tim Bastin <bastin.tim@gmail.com>
2025-03-18 16:29:08 +0100

    fixes tests

--- internal/core/context_utils_test.go
+++ internal/core/context_utils_test.go
@@ -19,38 +19,4 @@ func TestSortQuery(t *testing.T) {
 
 		assert.Equal(t, `"single" asc`, sql)
 	})
-
-	t.Run("should snake case the field", func(t *testing.T) {
-		q := core.SortQuery{
-			Field:    "camelCase",
-			Operator: "asc",
-		}
-
-		sql := q.SQL()
-
-		assert.Equal(t, `"camel_case" asc`, sql)
-	})
-
-	t.Run("should snake case relation fields", func(t *testing.T) {
-		q := core.SortQuery{
-			Field:    "relationField.camelCase",
-			Operator: "desc",
-		}
-
-		sql := q.SQL()
-
-		assert.Equal(t, `"relation_field"."camel_case" desc NULLS LAST`, sql)
-	})
-
-	t.Run("should respect nested relation fields", func(t *testing.T) {
-
-		q := core.SortQuery{
-			Field:    "component.project.scoreCardScore",
-			Operator: "asc",
-		}
-
-		sql := q.SQL()
-
-		assert.Equal(t, `"Component__Project"."score_card_score" asc`, sql)
-	})
 }
