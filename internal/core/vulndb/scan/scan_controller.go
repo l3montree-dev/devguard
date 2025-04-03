af7e22062b9ba5f829ec9ed2d1391c0cb5e2389c
Rafi <refaei.shikho@hotmail.com>
2025-04-03 09:16:58 +0200

    remove goroutine from scan controller
    Signed-off-by: Rafi <refaei.shikho@hotmail.com>

--- internal/core/vulndb/scan/scan_controller.go
+++ internal/core/vulndb/scan/scan_controller.go
@@ -135,17 +135,15 @@ func DependencyVulnScan(c core.Context, bom normalize.SBOM, s *httpController) (
 
 	//Check if we want to create an issue for this assetVersion
 	if s.dependencyVulnService.ShouldCreateIssues(assetVersion) {
-		go func() {
-			err := s.dependencyVulnService.CreateIssuesForVulnsIfThresholdExceeded(asset, opened)
-			if err != nil {
-				slog.Error("could not create issues for vulnerabilities", "err", err)
-			}
-			// close the fixed vulnerabilities
-			err = s.dependencyVulnService.CloseIssuesAsFixed(asset, closed)
-			if err != nil {
-				slog.Error("could not close issues for vulnerabilities", "err", err)
-			}
-		}()
+		err := s.dependencyVulnService.CreateIssuesForVulnsIfThresholdExceeded(asset, opened)
+		if err != nil {
+			slog.Error("could not create issues for vulnerabilities", "err", err)
+		}
+		// close the fixed vulnerabilities
+		err = s.dependencyVulnService.CloseIssuesAsFixed(asset, closed)
+		if err != nil {
+			slog.Error("could not close issues for vulnerabilities", "err", err)
+		}
 	}
 
 	// we need to do ticket sync here to ensure that exiting vulns (not with scan opened) are synced
@@ -167,6 +165,7 @@ func DependencyVulnScan(c core.Context, bom normalize.SBOM, s *httpController) (
 			return scanResults, err
 		}
 	}
+
 	scanResults.AmountOpened = len(opened) //Fill in the results
 	scanResults.AmountClosed = len(closed)
 	scanResults.DependencyVulns = utils.Map(newState, dependency_vuln.DependencyVulnToDto)
