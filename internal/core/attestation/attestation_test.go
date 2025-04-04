247f8482437f007269f20a436186af1c93fdb336
patrick.rissmann@l3montree.com <patrickrissmann@MacBook-Pro-von-Patrick.local>
2025-04-04 16:44:17 +0200

    forgot to add the new files

---
+++ internal/core/attestation/attestation_test.go
@@ -0,0 +1,56 @@
+package attestation_test
+
+import (
+	"bytes"
+	"fmt"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/l3montree-dev/devguard/internal/core"
+	"github.com/l3montree-dev/devguard/internal/core/attestation"
+	"github.com/l3montree-dev/devguard/internal/database/models"
+	"github.com/l3montree-dev/devguard/mocks"
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestList(t *testing.T) {
+	t.Run("everything works as expected", func(t *testing.T) {
+		req := httptest.NewRequest("GET", "/webhook", bytes.NewBufferString(""))
+		e := echo.New()
+		ctx := e.NewContext(req, httptest.NewRecorder())
+
+		asset := models.Asset{}
+		core.SetAsset(ctx, asset)
+
+		attestationRepository := mocks.NewCoreAttestationRepository(t)
+		attestationRepository.On("GetByAssetID", mock.Anything).Return([]models.Attestation{
+			{AttestationName: "not ocol name"},
+		}, nil)
+		attestationController := attestation.NewAttestationController(attestationRepository)
+		result := attestationController.List(ctx)
+		if result != nil {
+			t.Fail()
+		}
+
+	})
+	t.Run("getByAssetID returns an error so we should also receive an error", func(t *testing.T) {
+		req := httptest.NewRequest("GET", "/webhook", bytes.NewBufferString(""))
+		e := echo.New()
+		ctx := e.NewContext(req, httptest.NewRecorder())
+
+		asset := models.Asset{}
+		core.SetAsset(ctx, asset)
+
+		attestationRepository := mocks.NewCoreAttestationRepository(t)
+		attestationRepository.On("GetByAssetID", mock.Anything).Return([]models.Attestation{}, fmt.Errorf("Something went wrong"))
+		attestationController := attestation.NewAttestationController(attestationRepository)
+
+		result := attestationController.List(ctx)
+
+		if result == nil {
+			t.Fail()
+		}
+
+	})
+}
