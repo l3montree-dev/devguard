cc5eacb2c8589c534b0399798c182248ccb39834
Tim Bastin <bastin.tim@gmail.com>
2025-04-10 13:03:46 +0200

    fixes tests

--- internal/core/integrations/thirdparty_integration_test.go
+++ internal/core/integrations/thirdparty_integration_test.go
@@ -67,7 +67,29 @@ func TestRenderPathToComponent(t *testing.T) {
 		}
 
 		//String for the empty graph + 1 node being root with a linebreak
-		assert.Equal(t, "```mermaid \n %%{init: { 'theme':'dark' } }%%\n flowchart TD\nroot --> \nnode0[testDependency] --> \nnode1[testPURL]\n```\n", result)
+		assert.Equal(t, "```mermaid \n %%{init: { 'theme':'dark' } }%%\n flowchart TD\nroot --> \nnode0[\"testDependency\"] --> \nnode1[\"testPURL\"]\n```\n", result)
+
+	})
+	t.Run("should escape @ symbols", func(t *testing.T) {
+		components := []models.ComponentDependency{
+			{ComponentPurl: nil, DependencyPurl: "testDependency"}, // root --> testDependency
+			{ComponentPurl: utils.Ptr("testomatL"), DependencyPurl: "testPURL"},
+			{ComponentPurl: utils.Ptr("testDependency"), DependencyPurl: "test@PURL"},
+		}
+		componentRepository := mocks.NewComponentRepository(t)
+		componentRepository.On("LoadPathToComponent", mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(components, nil)
+
+		assetID := uuid.New()
+		assetVersionName := "TestName"
+		scannerID := "SBOM-File-Upload"
+		pURL := "pkg:npm:test"
+
+		result, err := renderPathToComponent(componentRepository, assetID, assetVersionName, scannerID, pURL)
+		if err != nil {
+			t.Fail()
+		}
+
+		assert.Equal(t, "```mermaid \n %%{init: { 'theme':'dark' } }%%\n flowchart TD\nroot --> \nnode0[\"testDependency\"] --> \nnode1[\"test\\@PURL\"]\n```\n", result)
 
 	})
 }
