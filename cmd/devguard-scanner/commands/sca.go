79459da453ca171a0b1aacb9bae4ca8fb28fc2a4
Tim Bastin <bastin.tim@gmail.com>
2025-04-16 19:09:48 +0200

    using camelCase for flag names

--- cmd/devguard-scanner/commands/sca.go
+++ cmd/devguard-scanner/commands/sca.go
@@ -314,10 +314,8 @@ func addScanFlags(cmd *cobra.Command) {
 		return
 	}
 
-	cmd.Flags().Bool("riskManagement", true, "Enable risk management (stores the detected vulnerabilities in devguard)")
-
 	cmd.Flags().String("path", ".", "The path to the project to scan. Defaults to the current directory.")
-	cmd.Flags().String("fail-on-risk", "critical", "The risk level to fail the scan on. Can be 'low', 'medium', 'high' or 'critical'. Defaults to 'critical'.")
+	cmd.Flags().String("failOnRisk", "critical", "The risk level to fail the scan on. Can be 'low', 'medium', 'high' or 'critical'. Defaults to 'critical'.")
 	cmd.Flags().String("webUI", "https://main.devguard.org", "The url of the web UI to show the scan results in. Defaults to 'https://app.devguard.dev'.")
 
 }
