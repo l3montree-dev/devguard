a4bb8409e542ada54969fbfb5c91db094b8dcba0
Tim Bastin <bastin.tim@gmail.com>
2025-04-22 15:30:11 +0200

    fixes wrong path joining in iac.go

--- cmd/devguard-scanner/commands/iac.go
+++ cmd/devguard-scanner/commands/iac.go
@@ -6,16 +6,17 @@ import (
 	"log/slog"
 	"os"
 	"os/exec"
+	"path"
 
 	"github.com/l3montree-dev/devguard/internal/common"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 )
 
-func iacScan(path string) (*common.SarifResult, error) {
+func iacScan(p string) (*common.SarifResult, error) {
 	// run checkov
 	dir := os.TempDir()
-	dir = dir + "iac"
+	dir = path.Join(dir, "iac")
 	// create new directory
 	err := os.MkdirAll(dir, 0755)
 	if err != nil {
@@ -23,9 +24,9 @@ func iacScan(path string) (*common.SarifResult, error) {
 	}
 
 	var scannerCmd *exec.Cmd
-	slog.Info("Starting iac scanning", "path", path)
+	slog.Info("Starting iac scanning", "path", p)
 
-	scannerCmd = exec.Command("checkov", "-d", path, "--output", "sarif", "--output-file-path", dir) // nolint:all // 	There is no security issue right here. This runs on the client. You are free to attack yourself
+	scannerCmd = exec.Command("checkov", "-d", p, "--output", "sarif", "--output-file-path", dir) // nolint:all // 	There is no security issue right here. This runs on the client. You are free to attack yourself
 	stderr := &bytes.Buffer{}
 	scannerCmd.Stderr = stderr
 	scannerCmd.Run() // nolint:errcheck
