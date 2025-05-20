225130c21ff4ea39f1c959c7052d469bcb8920f2
Hubtrick-Git <patrick.rissmann@l3montree.com>
2025-05-20 16:34:06 +0200

    added tests for the function to check if an user is authorized

--- internal/core/integrations/gitlab_integration.go
+++ internal/core/integrations/gitlab_integration.go
@@ -342,7 +342,7 @@ func (g *gitlabIntegration) HandleWebhook(ctx core.Context) error {
 			return err
 		}
 
-		isMember, err := IsUserAuthorized(event, client)
+		isMember, err := isUserAuthorized(event, client)
 		if err != nil {
 			return err
 		}
@@ -400,11 +400,10 @@ func (g *gitlabIntegration) ListRepositories(ctx core.Context) ([]core.Repositor
 }
 
 // Check if the user who comments on a ticket is authorized to use commands like /accept
-func IsUserAuthorized(event *gitlab.IssueCommentEvent, client core.GitlabClientFacade) (bool, error) {
+func isUserAuthorized(event *gitlab.IssueCommentEvent, client core.GitlabClientFacade) (bool, error) {
 	if event == nil || event.User == nil {
 		slog.Error("missing event data")
 		return false, fmt.Errorf("missing event data")
-
 	}
 	return client.IsProjectMember(context.TODO(), event.ProjectID, event.User.ID)
 }
