fd1f4b2bf86155612d15471872fc164aa7c51de1
Rafi <refaei.shikho@hotmail.com>
2025-05-27 16:51:40 +0200

    fix tests
    Signed-off-by: Rafi <refaei.shikho@hotmail.com>

--- cmd/devguard-scanner/commands/intoto/intoto_test.go
+++ cmd/devguard-scanner/commands/intoto/intoto_test.go
@@ -2,7 +2,18 @@ package intotocmd
 
 import "testing"
 
+func setEmptyEnvVars(t *testing.T) {
+	// Clear the environment variables to avoid conflicts
+	t.Setenv("CI_COMMIT_REF_NAME", "")
+	t.Setenv("CI_DEFAULT_BRANCH", "")
+	t.Setenv("CI_COMMIT_TAG", "")
+	t.Setenv("GITHUB_REF_NAME", "")
+	t.Setenv("GITHUB_BASE_REF", "")
+}
+
 func TestRedactSecret(t *testing.T) {
+
+	setEmptyEnvVars(t)
 
 	t.Run("RedactSecret", func(t *testing.T) {
 		testStr := "devguard-scanner intoto stop --step=build --products=image-digest.txt --products=image-tag.txt --products=image-tag.txt --token=20b232bf8fde72ebdc2e6e7fe599de4325bdad71b555e2d35e7df8783b4e4e54 --apiUrl=https://api.main.devguard.org"
