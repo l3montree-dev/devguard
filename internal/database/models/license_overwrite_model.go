52b60fea58b6c0beac50f5cb70f489abc3500625
Hubtrick-Git <patrick.rissmann@l3montree.com>
2025-05-27 16:48:04 +0200

    overhauled json keys

--- internal/database/models/license_overwrite_model.go
+++ internal/database/models/license_overwrite_model.go
@@ -3,7 +3,7 @@ package models
 import "github.com/google/uuid"
 
 type LicenseOverwrite struct {
-	License_id     string    `json:"license_id" gorm:"primarykey;type:text"`
+	License_id     string    `json:"licenseId" gorm:"primarykey;type:text"`
 	OrganizationId uuid.UUID `json:"organizationId" gorm:"type:uuid;foreignKey:OrganizationID;references:ID;"`
 	ComponentPurl  string    `json:"componentPurl" gorm:"type:text"`
 	Justification  string    `json:"justification" gorm:"type:text"`
