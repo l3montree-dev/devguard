8e7b70acc0a2b99b9528b2b8001fc81619ff7d70
Hubtrick-Git <patrick.rissmann@l3montree.com>
2025-05-27 14:47:04 +0200

    added create method to controller

--- internal/core/component/license_overwrite_controller.go
+++ internal/core/component/license_overwrite_controller.go
@@ -4,6 +4,7 @@ import (
 	"github.com/google/uuid"
 	"github.com/l3montree-dev/devguard/internal/core"
 	"github.com/l3montree-dev/devguard/internal/database/models"
+	"github.com/labstack/echo/v4"
 	"github.com/package-url/packageurl-go"
 )
 
@@ -28,4 +29,23 @@ func (controller LicenseOverwriteController) GetComponentOverwriteForOrganizatio
 		return result, err
 	}
 	return result, nil
+}
+
+func (controller LicenseOverwriteController) Create(ctx core.Context) error {
+	var newLicenseOverwrite models.LicenseOverwrite
+	if err := ctx.Bind(&newLicenseOverwrite); err != nil {
+		return echo.NewHTTPError(400, "unable to process request").WithInternal(err)
+	}
+
+	if err := core.V.Struct(newLicenseOverwrite); err != nil {
+		return echo.NewHTTPError(400, err.Error())
+	}
+	if newLicenseOverwrite.License_id == "" {
+		return echo.NewHTTPError(400, "license id must not be empty")
+	}
+	err := controller.LicenseOverwriteRepository.Create(nil, &newLicenseOverwrite)
+	if err != nil {
+		return echo.NewHTTPError(500, err.Error())
+	}
+	return ctx.JSON(200, newLicenseOverwrite)
 }
