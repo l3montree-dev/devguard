97299a18da47349fdae255d34b5fe5ad2568ccd8
Hubtrick-Git <patrick.rissmann@l3montree.com>
2025-05-27 13:52:31 +0200

    added function to controller to retrieve a possible LO

--- internal/core/component/license_overwrite_controller.go
+++ internal/core/component/license_overwrite_controller.go
@@ -1,7 +1,10 @@
 package component
 
 import (
+	"github.com/google/uuid"
 	"github.com/l3montree-dev/devguard/internal/core"
+	"github.com/l3montree-dev/devguard/internal/database/models"
+	"github.com/package-url/packageurl-go"
 )
 
 type LicenseOverwriteController struct {
@@ -12,4 +15,17 @@ func NewLicenseOverwriteController(licenseOverwriteRepository core.LicenseOverwr
 	return &LicenseOverwriteController{
 		LicenseOverwriteRepository: licenseOverwriteRepository,
 	}
+}
+
+func (controller LicenseOverwriteController) GetComponentOverwriteForOrganization(org_id uuid.UUID, pURL string) (models.LicenseOverwrite, error) {
+	var result models.LicenseOverwrite
+	valid_purl, err := packageurl.FromString(pURL)
+	if err != nil {
+		return result, err
+	}
+	result, err = controller.LicenseOverwriteRepository.MaybeGetOverwriteForComponent(org_id, valid_purl)
+	if err != nil {
+		return result, err
+	}
+	return result, nil
 }
