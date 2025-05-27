b7000ddfc2702ebafc089689e0730c6d4b95f148
Hubtrick-Git <patrick.rissmann@l3montree.com>
2025-05-27 13:35:01 +0200

    added two functions to retirve license overwrites, to the repository

---
+++ internal/database/repositories/license_overwrite_repository.go
@@ -0,0 +1,32 @@
+package repositories
+
+import (
+	"github.com/google/uuid"
+	"github.com/l3montree-dev/devguard/internal/common"
+	"github.com/l3montree-dev/devguard/internal/core"
+	"github.com/l3montree-dev/devguard/internal/database/models"
+	"gorm.io/gorm"
+)
+
+type LicenseOverwriteRepository struct {
+	common.Repository[string, models.LicenseOverwrite, core.DB]
+	db *gorm.DB
+}
+
+func (r *LicenseOverwriteRepository) GetAllOverwritesForOrganization(orgID uuid.UUID) ([]models.LicenseOverwrite, error) {
+	var result []models.LicenseOverwrite
+	err := r.db.Where("organization_id = ?", orgID).Find(&result).Error
+	if err != nil {
+		return result, err
+	}
+	return result, nil
+}
+
+func (r *LicenseOverwriteRepository) MaybeGetOverwriteForComponent(orgID uuid.UUID, pURL string) (models.LicenseOverwrite, error) {
+	var result models.LicenseOverwrite
+	err := r.db.Where("organization_id = ? AND component_purl = ?", orgID, pURL).Find(&result).Error
+	if err != nil {
+		return result, err
+	}
+	return result, nil
+}
