706b7b946f5b64606536f94a6d587503022c5347
Hubtrick-Git <patrick.rissmann@l3montree.com>
2025-06-04 12:39:44 +0200

    i fixed some major issues with tims code :D

---
+++ internal/core/integrations/gitlabint/gitlab_client_factory.go
@@ -0,0 +1,90 @@
+package gitlabint
+
+import (
+	"fmt"
+
+	"github.com/google/uuid"
+	"github.com/l3montree-dev/devguard/internal/common"
+	"github.com/l3montree-dev/devguard/internal/core"
+	"github.com/l3montree-dev/devguard/internal/database/models"
+	"github.com/l3montree-dev/devguard/internal/utils"
+	"github.com/pkg/errors"
+	gitlab "gitlab.com/gitlab-org/api/client-go"
+)
+
+type gitlabClientFactory interface {
+	FromIntegration(integration models.GitLabIntegration) (gitlabClientFacade, error)
+	FromIntegrationUUID(id uuid.UUID) (gitlabClientFacade, error)
+	FromOauth2Token(token models.GitLabOauth2Token, enableClientCache bool) (gitlabClientFacade, error)
+	FromAccessToken(accessToken string, baseUrl string) (gitlabClientFacade, error)
+}
+
+type SimpleGitlabClientFactory struct {
+	gitlabIntegrationRepository core.GitlabIntegrationRepository
+	oauth2GitlabIntegration     map[string]*GitlabOauth2Config
+}
+
+func NewGitlabClientFactory(gitlabIntegrationRepository core.GitlabIntegrationRepository, oauth2GitlabIntegration map[string]*GitlabOauth2Config) SimpleGitlabClientFactory {
+	return SimpleGitlabClientFactory{
+		gitlabIntegrationRepository: gitlabIntegrationRepository,
+		oauth2GitlabIntegration:     oauth2GitlabIntegration,
+	}
+}
+
+func (factory SimpleGitlabClientFactory) FromIntegration(integration models.GitLabIntegration) (gitlabClientFacade, error) {
+	// Use installation transport with client.
+	client, err := gitlab.NewClient(integration.AccessToken, gitlab.WithBaseURL(integration.GitLabUrl))
+	if err != nil {
+		return gitlabClient{}, err
+	}
+
+	return gitlabClient{
+		Client:   client,
+		clientID: integration.ID.String(),
+	}, nil
+}
+
+func (factory SimpleGitlabClientFactory) FromIntegrationUUID(id uuid.UUID) (gitlabClientFacade, error) {
+	integration, err := factory.gitlabIntegrationRepository.Read(id)
+	if err != nil {
+		return nil, err
+	}
+
+	return factory.FromIntegration(integration)
+}
+
+func (factory SimpleGitlabClientFactory) FromOauth2Token(token models.GitLabOauth2Token, enableClientCache bool) (gitlabClientFacade, error) {
+	// get the correct gitlab oauth2 integration configuration
+	for _, integration := range factory.oauth2GitlabIntegration {
+		if integration.ProviderID == token.ProviderID {
+			oauth2Client := integration.client(token)
+			if enableClientCache {
+				common.WrapHTTPClient(oauth2Client, httpClientCache.Handler())
+			}
+
+			client, err := gitlab.NewClient(token.AccessToken, gitlab.WithHTTPClient(oauth2Client), gitlab.WithBaseURL(integration.GitlabBaseURL))
+			if err != nil {
+				return gitlabOauth2Client{}, err
+			}
+
+			return gitlabOauth2Client{
+				gitlabUserID: token.GitLabUserID,
+				gitlabClient: gitlabClient{Client: client, clientID: fmt.Sprintf("oauth2-%s", token.ID.String()), gitProviderID: utils.Ptr(integration.ProviderID)}}, nil
+		}
+	}
+	return nil, errors.New("could not find gitlab oauth2 integration")
+}
+
+func (factory SimpleGitlabClientFactory) FromAccessToken(accessToken string, baseUrl string) (gitlabClientFacade, error) {
+	if accessToken == "" {
+		return nil, errors.New("access token is empty")
+	}
+	if baseUrl == "" {
+		return nil, errors.New("base URL is empty")
+	}
+	client, err := gitlab.NewClient(accessToken, gitlab.WithBaseURL(baseUrl))
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create gitlab client")
+	}
+	return gitlabClient{Client: client}, nil
+}
--- internal/core/integrations/gitlabint/gitlab_integration.go
+++ internal/core/integrations/gitlabint/gitlab_integration.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"encoding/json"
 	"os"
-	"time"
 
 	"github.com/labstack/echo/v4"
 	"github.com/pkg/errors"
@@ -182,6 +181,7 @@ func NewGitLabIntegration(oauth2GitlabIntegration map[string]*GitlabOauth2Config
 		projectService:              projectService,
 		assetService:                assetService,
 		casbinRBACProvider:          casbinRBACProvider,
+<<<<<<< Updated upstream
 
 		gitlabClientFactory: func(id uuid.UUID) (gitlabClientFacade, error) {
 			integration, err := gitlabIntegrationRepository.Read(id)
@@ -207,6 +207,9 @@ func NewGitLabIntegration(oauth2GitlabIntegration map[string]*GitlabOauth2Config
 			}
 			return nil, errors.New("could not find gitlab oauth2 integration")
 		},
+=======
+		clientFactory:               clientFactory,
+>>>>>>> Stashed changes
 	}
 }
 
@@ -334,20 +337,11 @@ func (g *GitlabIntegration) getAndSaveOauth2TokenFromAuthServer(ctx core.Context
 		})
 	}
 
-	nonExpiredTokens := make([]models.GitLabOauth2Token, 0, len(tokenSlice))
-	// filter out expired tokens
-	for _, token := range tokenSlice {
-		if token.Expiry.After(time.Now()) {
-			nonExpiredTokens = append(nonExpiredTokens, token)
-		}
-	}
-
-	// save the tokens to the database
-	if len(nonExpiredTokens) != 0 {
-		err = g.gitlabOauth2TokenRepository.Save(nil, utils.SlicePtr(nonExpiredTokens)...)
+	// save the oauth2 tokens if the user doesnt have any tokens yet
+	if len(tokenSlice) > 0 {
+		err := g.gitlabOauth2TokenRepository.CreateIfNotExists(utils.SlicePtr(tokenSlice))
 		if err != nil {
-			// if an error happens, just swallow it
-			return tokenSlice, nil
+			return nil, err
 		}
 	}
 
