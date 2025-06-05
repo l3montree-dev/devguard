4f4c62002ef0503b707afaf1bf6eb10012012128
Hubtrick-Git <patrick.rissmann@l3montree.com>
2025-06-05 18:18:52 +0200

    reverting sbom test for this branch

--- internal/core/assetversion/asset_version_controller_test.go
+++ internal/core/assetversion/asset_version_controller_test.go
@@ -2,19 +2,8 @@ package assetversion_test
 
 import (
 	"testing"
-
-	"github.com/l3montree-dev/devguard/integration_tests"
-	"github.com/l3montree-dev/devguard/internal/inithelper"
-	"github.com/labstack/echo/v4"
 )
 
 func TestBuildSBOM(t *testing.T) {
-	db, terminate := integration_tests.InitDatabaseContainer("../../../../initdb.sql")
-	defer terminate()
-	assetVersionController := inithelper.CreateAssetVersionController(db, nil, nil, integration_tests.TestGitlabClientFactory{GitlabClientFacade: nil}, nil)
-	app := echo.New()
-	ctx := app.NewContext(nil, nil)
-	t.Run("default test", func(t *testing.T) {
-		assetVersionController.SBOMJSON(ctx)
-	})
+
 }
