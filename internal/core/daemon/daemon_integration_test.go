2978ceca0ba1daa3ea91ecd6e726c0255cd47e47
Rafi <refaei.shikho@hotmail.com>
2025-06-05 16:56:16 +0200

    add component properties daemon test
    Signed-off-by: Rafi <refaei.shikho@hotmail.com>

--- internal/core/daemon/daemon_integration_test.go
+++ internal/core/daemon/daemon_integration_test.go
@@ -430,3 +430,243 @@ func TestDaemonRecalculateRisk(t *testing.T) {
 		assert.Greater(t, *updatedDependencyVuln.RawRiskAssessment, oldRawRiskValue)
 	})
 }
+
+func TestDaemonComponentProperties(t *testing.T) {
+	db, terminate := integration_tests.InitDatabaseContainer("../../../initdb.sql")
+	defer terminate()
+
+	err := db.AutoMigrate(
+		&models.Org{},
+		&models.Project{},
+		&models.AssetVersion{},
+		&models.Asset{},
+		&models.ComponentDependency{},
+		&models.Component{},
+		&models.CVE{},
+		&models.AffectedComponent{},
+		&models.DependencyVuln{},
+		&models.Exploit{},
+	)
+	assert.Nil(t, err)
+
+	os.Setenv("FRONTEND_URL", "FRONTEND_URL")
+
+	org, project, asset := integration_tests.CreateOrgProjectAndAsset(db)
+
+	org.Slug = "org-slug"
+	err = db.Save(&org).Error
+	assert.Nil(t, err)
+	project.Slug = "project-slug"
+	err = db.Save(&project).Error
+	assert.Nil(t, err)
+
+	assetVersion := models.AssetVersion{
+		Name:          "main",
+		AssetID:       asset.ID,
+		DefaultBranch: true,
+	}
+	err = db.Create(&assetVersion).Error
+	assert.Nil(t, err)
+
+	componentA := models.Component{
+		Purl:          "pkg:npm/react@18.2.0",
+		ComponentType: models.ComponentTypeLibrary,
+		Version:       "18.2.0",
+	}
+	err = db.Create(&componentA).Error
+	assert.Nil(t, err)
+
+	componentB := models.Component{
+		Purl:          "pkg:npm/react-dom@15.0.0",
+		ComponentType: models.ComponentTypeLibrary,
+		Version:       "15.0.0",
+	}
+	err = db.Create(&componentB).Error
+	assert.Nil(t, err)
+	devguardScanner := "github.com/l3montree-dev/devguard/cmd/devguard-scanner" + "/"
+	componentDependencyA := models.ComponentDependency{
+		AssetID:          asset.ID,
+		AssetVersionName: assetVersion.Name,
+		AssetVersion:     assetVersion,
+		ScannerIDs:       devguardScanner + "sca",
+		ComponentPurl:    nil,
+		DependencyPurl:   "pkg:npm/react@18.2.0",
+		Dependency:       componentA,
+	}
+	err = db.Create(&componentDependencyA).Error
+	assert.Nil(t, err)
+	componentDependencyB := models.ComponentDependency{
+		AssetID:          asset.ID,
+		AssetVersionName: assetVersion.Name,
+		AssetVersion:     assetVersion,
+		ScannerIDs:       devguardScanner + "sca",
+		ComponentPurl:    &componentA.Purl,
+		DependencyPurl:   "pkg:npm/react-dom@15.0.0",
+		Dependency:       componentB,
+	}
+	err = db.Create(&componentDependencyB).Error
+	assert.Nil(t, err)
+
+	affectedComponent := models.AffectedComponent{
+		ID:                 "1",
+		PurlWithoutVersion: "pkg:npm/react-dom",
+		Version:            utils.Ptr("15.0.0"),
+		CVE:                []models.CVE{{CVE: "CVE-2025-46569"}},
+	}
+	err = db.Create(&affectedComponent).Error
+	assert.Nil(t, err)
+
+	cve := models.CVE{
+		CVE:  "CVE-2025-46569",
+		CVSS: 8.0,
+		AffectedComponents: []*models.AffectedComponent{{
+			ID: "1",
+		}},
+	}
+	err = db.Save(&cve).Error
+	assert.Nil(t, err)
+
+	dependencyVuln := models.DependencyVuln{
+		Vulnerability: models.Vulnerability{
+			ID:               "1",
+			AssetID:          asset.ID,
+			AssetVersion:     assetVersion,
+			AssetVersionName: assetVersion.Name,
+			ScannerIDs:       "github.com/l3montree-dev/devguard/cmd/devguard-scanner/sca",
+			State:            models.VulnStateOpen,
+			LastDetected:     time.Now(),
+		},
+
+		CVE:               &cve,
+		CVEID:             utils.Ptr(cve.CVE),
+		ComponentDepth:    utils.Ptr(3), //this is a wrong value, it should be updated by the daemon
+		ComponentPurl:     utils.Ptr("pkg:npm/react-dom@15.0.0"),
+		RawRiskAssessment: utils.Ptr(8.0),
+	}
+	err = db.Create(&dependencyVuln).Error
+	assert.Nil(t, err)
+	assert.Nil(t, dependencyVuln.ComponentFixedVersion)
+
+	t.Run("should update the component properties, including fixed version and component depth", func(t *testing.T) {
+		fixedVersion := "15.0.1"
+		dependencyVuln.ComponentFixedVersion = &fixedVersion
+		err = db.Save(&dependencyVuln).Error
+		assert.Nil(t, err)
+
+		err = daemon.UpdateComponentProperties(db)
+		assert.Nil(t, err)
+
+		var updatedDependencyVuln models.DependencyVuln
+		err = db.First(&updatedDependencyVuln, "asset_id = ? AND asset_version_name = ?", asset.ID, assetVersion.Name).Error
+		assert.Nil(t, err)
+
+		assert.NotNil(t, updatedDependencyVuln.ComponentFixedVersion)
+		assert.Equal(t, fixedVersion, *updatedDependencyVuln.ComponentFixedVersion)
+		//componentB is the affected component
+		//componentA -> componentB
+		assert.Equal(t, 1, *updatedDependencyVuln.ComponentDepth)
+
+	})
+
+	t.Run("should update the component depth to a lower value, if a component dependency is found with a shorter path, and the scanner is only container-scanning or sca", func(t *testing.T) {
+		componentDependencyC := models.ComponentDependency{
+			AssetID:          asset.ID,
+			AssetVersionName: assetVersion.Name,
+			AssetVersion:     assetVersion,
+			ScannerIDs:       devguardScanner + "sca",
+			ComponentPurl:    nil,
+			DependencyPurl:   "pkg:npm/react-dom@15.0.0",
+			Dependency:       componentB,
+		}
+		err = db.Create(&componentDependencyC).Error
+		assert.Nil(t, err)
+
+		err = daemon.UpdateComponentProperties(db)
+		assert.Nil(t, err)
+
+		var updatedDependencyVuln models.DependencyVuln
+		err = db.First(&updatedDependencyVuln, "asset_id = ? AND asset_version_name = ? AND cve_id = ?", asset.ID, assetVersion.Name, cve.CVE).Error
+		assert.Nil(t, err)
+
+		assert.NotNil(t, updatedDependencyVuln.ComponentDepth)
+		assert.Equal(t, 0, *updatedDependencyVuln.ComponentDepth)
+
+		//delete the component dependency
+		err = db.Delete(&componentDependencyC).Error
+		assert.Nil(t, err)
+
+		err = daemon.UpdateComponentProperties(db)
+		assert.Nil(t, err)
+		err = db.First(&updatedDependencyVuln, "asset_id = ? AND asset_version_name = ? AND cve_id = ?", asset.ID, assetVersion.Name, cve.CVE).Error
+		assert.Nil(t, err)
+
+		assert.NotNil(t, updatedDependencyVuln.ComponentDepth)
+		assert.Equal(t, 1, *updatedDependencyVuln.ComponentDepth)
+
+	})
+
+	t.Run("should not update the component depth to shorter value, if a new component dependency has a shorter path but the container-scanning scanner is used additionally to the sca scanner", func(t *testing.T) {
+
+		dependencyVuln.ScannerIDs = "github.com/l3montree-dev/devguard/cmd/devguard-scanner/sca github.com/l3montree-dev/devguard/cmd/devguard-scanner/container-scanning"
+		err = db.Save(&dependencyVuln).Error
+		assert.Nil(t, err)
+
+		componentDependencyC := models.ComponentDependency{
+			AssetID:          asset.ID,
+			AssetVersionName: assetVersion.Name,
+			AssetVersion:     assetVersion,
+			ScannerIDs:       devguardScanner + "container-scanning",
+			ComponentPurl:    nil,
+			DependencyPurl:   "pkg:npm/react-dom@15.0.0",
+			Dependency:       componentB,
+		}
+		err = db.Create(&componentDependencyC).Error
+		assert.Nil(t, err)
+
+		err = daemon.UpdateComponentProperties(db)
+		assert.Nil(t, err)
+
+		var updatedDependencyVuln models.DependencyVuln
+		err = db.First(&updatedDependencyVuln, "asset_id = ? AND asset_version_name = ? AND cve_id = ?", asset.ID, assetVersion.Name, cve.CVE).Error
+		assert.Nil(t, err)
+
+		assert.NotNil(t, updatedDependencyVuln.ComponentDepth)
+		assert.Equal(t, 1, *updatedDependencyVuln.ComponentDepth)
+
+		//delete the component dependency
+		err = db.Delete(&componentDependencyC).Error
+		assert.Nil(t, err)
+
+	})
+
+	t.Run("should update the component depth to lower value, if a new component dependency has a shorter path and the container-scanning scanner is the only scanner used", func(t *testing.T) {
+
+		dependencyVuln.ScannerIDs = "github.com/l3montree-dev/devguard/cmd/devguard-scanner/container-scanning"
+		err = db.Save(&dependencyVuln).Error
+		assert.Nil(t, err)
+
+		componentDependencyC := models.ComponentDependency{
+			AssetID:          asset.ID,
+			AssetVersionName: assetVersion.Name,
+			AssetVersion:     assetVersion,
+			ScannerIDs:       devguardScanner + "container-scanning",
+			ComponentPurl:    nil,
+			DependencyPurl:   "pkg:npm/react-dom@15.0.0",
+			Dependency:       componentB,
+		}
+		err = db.Create(&componentDependencyC).Error
+		assert.Nil(t, err)
+
+		err = daemon.UpdateComponentProperties(db)
+		assert.Nil(t, err)
+
+		var updatedDependencyVuln models.DependencyVuln
+		err = db.First(&updatedDependencyVuln, "asset_id = ? AND asset_version_name = ? AND cve_id = ?", asset.ID, assetVersion.Name, cve.CVE).Error
+		assert.Nil(t, err)
+
+		assert.NotNil(t, updatedDependencyVuln.ComponentDepth)
+		assert.Equal(t, 0, *updatedDependencyVuln.ComponentDepth)
+
+	})
+
+}
