51b4c38867c2e73c6f92fa5be9aeaf7c6cea7570
Hubtrick-Git <patrick.rissmann@l3montree.com>
2025-06-06 13:48:21 +0200

    first sketch for the sbom test

--- internal/core/assetversion/asset_version_controller_test.go
+++ internal/core/assetversion/asset_version_controller_test.go
@@ -1,9 +1,48 @@
 package assetversion_test
 
 import (
+	"fmt"
+	"net/http/httptest"
+	"os"
 	"testing"
+
+	"github.com/l3montree-dev/devguard/integration_tests"
+	"github.com/l3montree-dev/devguard/internal/core"
+	"github.com/l3montree-dev/devguard/internal/database/models"
+	"github.com/l3montree-dev/devguard/internal/inithelper"
+	"github.com/l3montree-dev/devguard/mocks"
+	"github.com/labstack/echo/v4"
 )
 
 func TestBuildSBOM(t *testing.T) {
+	db, terminate := integration_tests.InitDatabaseContainer("../../../initdb.sql")
+	defer terminate()
+	app := echo.New()
+	os.Setenv("FRONTEND_URL", "FRONTEND_URL")
+	assetVersionController := inithelper.CreateAssetVersionController(db, nil, nil, integration_tests.TestGitlabClientFactory{GitlabClientFacade: nil}, nil)
+	org, project, asset := integration_tests.CreateOrgProjectAndAsset(db)
+	setupContext := func(ctx core.Context) {
+		authSession := mocks.NewAuthSession(t)
+		core.SetAsset(ctx, asset)
+		core.SetProject(ctx, project)
+		core.SetOrg(ctx, org)
+		core.SetSession(ctx, authSession)
+	}
 
+	t.Run("default test", func(t *testing.T) {
+		recorder := httptest.NewRecorder()
+		req := httptest.NewRequest("GET", "/sbom-json/", nil)
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("X-Scanner", "scanner-1")
+		ctx := app.NewContext(req, recorder)
+		setupContext(ctx)
+		core.SetAssetVersion(ctx, models.AssetVersion{})
+		err := assetVersionController.SBOMJSON(ctx)
+		if err != nil {
+			t.Fail()
+		}
+		var sbom []byte
+		fmt.Printf("\n------------Result------------\n%s\n-------------End--------------\n", sbom)
+
+	})
 }
