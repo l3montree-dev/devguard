63d458648961745bb041dcd2528881c82a527d43
Hubtrick-Git <patrick.rissmann@l3montree.com>
2025-06-06 16:51:30 +0200

    added function to fill db with components, overwrites and component porjects

--- internal/core/assetversion/asset_version_controller_test.go
+++ internal/core/assetversion/asset_version_controller_test.go
@@ -5,11 +5,14 @@ import (
 	"net/http/httptest"
 	"os"
 	"testing"
+	"time"
 
+	"github.com/google/uuid"
 	"github.com/l3montree-dev/devguard/integration_tests"
 	"github.com/l3montree-dev/devguard/internal/core"
 	"github.com/l3montree-dev/devguard/internal/database/models"
 	"github.com/l3montree-dev/devguard/internal/inithelper"
+	"github.com/l3montree-dev/devguard/internal/utils"
 	"github.com/l3montree-dev/devguard/mocks"
 	"github.com/labstack/echo/v4"
 )
@@ -28,6 +31,7 @@ func TestBuildSBOM(t *testing.T) {
 		core.SetOrg(ctx, org)
 		core.SetSession(ctx, authSession)
 	}
+	buildDatabase(db, org.ID)
 
 	t.Run("default test", func(t *testing.T) {
 		recorder := httptest.NewRecorder()
@@ -45,4 +49,54 @@ func TestBuildSBOM(t *testing.T) {
 		fmt.Printf("\n------------Result------------\n%s\n-------------End--------------\n", sbom)
 
 	})
+}
+
+func buildDatabase(db core.DB, orgID uuid.UUID) {
+	lo := models.LicenseOverwrite{
+		LicenseID:      "Apache-2.0",
+		OrganizationId: orgID,
+		ComponentPurl:  "pkg:golang/filippo.io/edwards25519@v1.1.0",
+		Justification:  "while the specifications place no limits on the magnitude or precision of JSON number literals, the widely used JavaScript implementation stores them as IEEE754 binary64 quantities. For interoperability, applications should avoid transmitting numbers that cannot be represented in this way, for example, 1E400 or 3.141592653589793238462643383279.",
+	}
+	err := db.Create(&lo).Error
+	if err != nil {
+		panic(err)
+	}
+	componentProject := models.ComponentProject{
+		ProjectKey:      "github.com/xyflow/xyflow",
+		StarsCount:      2993,
+		ForksCount:      282,
+		OpenIssuesCount: 0,
+		License:         "MIT",
+		ScoreCardScore:  utils.Ptr(5.2),
+	}
+	err = db.Create(&componentProject).Error
+	if err != nil {
+		panic(err)
+	}
+	component1 := models.Component{
+		Purl:             "pkg:npm/@xyflow/system@0.0.42",
+		ComponentType:    models.ComponentTypeLibrary,
+		Version:          "0.0.42",
+		License:          utils.Ptr("MIT"),
+		Published:        utils.Ptr(time.Now()),
+		ComponentProject: &componentProject,
+	}
+	err = db.Create(&component1).Error
+	if err != nil {
+		panic(err)
+	}
+	component2 := models.Component{
+		Purl:             "pkg:npm/@xyflow/react@12.3.0",
+		ComponentType:    models.ComponentTypeLibrary,
+		Version:          "12.3.0",
+		License:          utils.Ptr("MIT"),
+		Published:        utils.Ptr(time.Now()),
+		ComponentProject: &componentProject,
+	}
+	err = db.Create(&component2).Error
+	if err != nil {
+		panic(err)
+	}
+
 }
