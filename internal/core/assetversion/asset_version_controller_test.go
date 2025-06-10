9ac748464d4bbd16fdee905874680a74f6a14a1b
Hubtrick-Git <patrick.rissmann@l3montree.com>
2025-06-10 18:20:06 +0200

    added more tests to build up the default test including some edge cases like empty db or nvalid version

--- internal/core/assetversion/asset_version_controller_test.go
+++ internal/core/assetversion/asset_version_controller_test.go
@@ -1,12 +1,16 @@
 package assetversion_test
 
 import (
+	"encoding/json"
+	"fmt"
 	"io"
 	"net/http/httptest"
 	"os"
+	"strings"
 	"testing"
 	"time"
 
+	"github.com/CycloneDX/cyclonedx-go"
 	"github.com/google/uuid"
 	"github.com/l3montree-dev/devguard/integration_tests"
 	"github.com/l3montree-dev/devguard/internal/core"
@@ -18,6 +22,7 @@ import (
 )
 
 func TestBuildSBOM(t *testing.T) {
+	//Build up a foundation for all the upcoming tests
 	db, terminate := integration_tests.InitDatabaseContainer("../../../initdb.sql")
 	defer terminate()
 	app := echo.New()
@@ -30,9 +35,118 @@ func TestBuildSBOM(t *testing.T) {
 		core.SetOrg(*ctx, org)
 		core.SetAssetVersion(*ctx, assetVersion)
 	}
-	buildDatabase(db, org.ID, asset.ID, "main")
+	t.Run("test with empty db", func(t *testing.T) {
+		recorder := httptest.NewRecorder()
+		req := httptest.NewRequest("GET", "/sbom-json/", nil)
+		ctx := app.NewContext(req, recorder)
+		setupContext(&ctx)
+		err := assetVersionController.SBOMJSON(ctx)
+		if err != nil {
+			t.Fail()
+		}
 
-	t.Run("default test", func(t *testing.T) {
+		//Process the results into an BOM
+		resp := recorder.Result()
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			t.Fail()
+		}
+		var BOMResult cyclonedx.BOM
+		if err = json.Unmarshal(body, &BOMResult); err != nil {
+			t.Fail()
+		}
+
+		fmt.Printf("\n%s\n", string(body))
+		//Test the bom
+		assert.Empty(t, BOMResult.Components)
+		assert.Empty(t, BOMResult.Dependencies)
+		assert.Equal(t, "Test Org", BOMResult.Metadata.Component.Author)
+		assert.Equal(t, "github.com/l3montree-dev/devguard", BOMResult.Metadata.Component.Publisher)
+		assert.Equal(t, "main", BOMResult.Metadata.Component.BOMRef)
+		assert.Equal(t, "latest", BOMResult.Metadata.Component.Version)
+	})
+	t.Run("test with only components in the db with no specific version", func(t *testing.T) {
+		createComponents(db, org.ID, asset.ID, assetVersion.Name)
+		recorder := httptest.NewRecorder()
+		req := httptest.NewRequest("GET", "/sbom-json/", nil)
+		ctx := app.NewContext(req, recorder)
+		setupContext(&ctx)
+		err := assetVersionController.SBOMJSON(ctx)
+		if err != nil {
+			t.Fail()
+		}
+
+		//Process the results into an BOM
+		resp := recorder.Result()
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			t.Fail()
+		}
+		var BOMResult cyclonedx.BOM
+		if err = json.Unmarshal(body, &BOMResult); err != nil {
+			t.Fail()
+		}
+
+		fmt.Printf("\n%s\n", string(body))
+		//Test the bom
+		assert.Empty(t, BOMResult.Components)
+		assert.Empty(t, BOMResult.Dependencies)
+		assert.Equal(t, "Test Org", BOMResult.Metadata.Component.Author)
+		assert.Equal(t, "github.com/l3montree-dev/devguard", BOMResult.Metadata.Component.Publisher)
+		assert.Equal(t, "main", BOMResult.Metadata.Component.BOMRef)
+		assert.Equal(t, "latest", BOMResult.Metadata.Component.Version)
+	})
+	t.Run("test with only components in the db with an invalid version set", func(t *testing.T) {
+		createComponents(db, org.ID, asset.ID, assetVersion.Name)
+		recorder := httptest.NewRecorder()
+		req := httptest.NewRequest("GET", "/sbom-json/", nil)
+		ctx := app.NewContext(req, recorder)
+		setupContext(&ctx)
+		params := ctx.QueryParams()
+		params.Add("version", "special version")
+
+		err := assetVersionController.SBOMJSON(ctx)
+		if err == nil {
+			t.Fail()
+		}
+	})
+	t.Run("test with only components in the db with a valid version set", func(t *testing.T) {
+		createComponents(db, org.ID, asset.ID, assetVersion.Name)
+		recorder := httptest.NewRecorder()
+		req := httptest.NewRequest("GET", "/sbom-json/", nil)
+		ctx := app.NewContext(req, recorder)
+		setupContext(&ctx)
+		params := ctx.QueryParams()
+		params.Add("version", "2.1.9")
+
+		err := assetVersionController.SBOMJSON(ctx)
+		if err != nil {
+			t.Fail()
+		}
+
+		//Process the results into an BOM
+		resp := recorder.Result()
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			t.Fail()
+		}
+		var BOMResult cyclonedx.BOM
+		if err = json.Unmarshal(body, &BOMResult); err != nil {
+			t.Fail()
+		}
+
+		fmt.Printf("\n%s\n", string(body))
+		//Test the bom
+		assert.Empty(t, BOMResult.Components)
+		assert.Empty(t, BOMResult.Dependencies)
+		assert.Equal(t, "Test Org", BOMResult.Metadata.Component.Author)
+		assert.Equal(t, "github.com/l3montree-dev/devguard", BOMResult.Metadata.Component.Publisher)
+		assert.Equal(t, "main", BOMResult.Metadata.Component.BOMRef)
+		assert.Equal(t, "2.1.9", BOMResult.Metadata.Component.Version)
+	})
+	/*t.Run("create a normal sbom with components and dependencies and a license overwrite ", func(t *testing.T) {
+		//Setup environment for this test
+		buildDatabase(db, org.ID, asset.ID, "main", "with license overwrite, components and dependencies")
 		recorder := httptest.NewRecorder()
 		req := httptest.NewRequest("GET", "/sbom-json/", nil)
 		req.Header.Set("Content-Type", "application/json")
@@ -43,33 +157,23 @@ func TestBuildSBOM(t *testing.T) {
 		if err != nil {
 			t.Fail()
 		}
+
+		//Process the results into an BOM
 		resp := recorder.Result()
 		body, err := io.ReadAll(resp.Body)
 		if err != nil {
 			t.Fail()
 		}
-		/*jsonSBOM, err := json.Marshal(body)
-		if err != nil {
+		var jsonBOM cyclonedx.BOM
+		if err = json.Unmarshal(body, &jsonBOM); err != nil {
 			t.Fail()
-		}*/
-		cutBody := string(body)[:82] + string(body)[102:]
+		}
 
-		assert.Equal(t, `{"bomFormat":"CycloneDX","specVersion":"1.5","version":1,"metadata":{"timestamp":"","component":{"bom-ref":"main","type":"application","author":"Test Org","publisher":"github.com/l3montree-dev/devguard","name":"main","version":"latest"}},"components":[{"bom-ref":"pkg:npm/@xyflow/react@12.3.0","type":"library","name":"@xyflow/react","version":"12.3.0","licenses":[{"license":{"id":"MIT","name":"MIT"}}],"purl":"pkg:npm/@xyflow/react@12.3.0"},{"bom-ref":"pkg:npm/@xyflow/system@0.0.42","type":"library","name":"@xyflow/system","version":"0.0.42","licenses":[{"license":{"id":"Apache-2.0"}}],"purl":"pkg:npm/@xyflow/system@0.0.42"}],"dependencies":[{"ref":"pkg:npm/@xyflow/system@0.0.42","dependsOn":["pkg:npm/@xyflow/react@12.3.0"]},{"ref":"main","dependsOn":["pkg:npm/@xyflow/system@0.0.42"]}]}`, cutBody[:len(cutBody)-1])
+		//Test the BOM
 
-	})
+	})*/
 }
-
-func buildDatabase(db core.DB, orgID uuid.UUID, assetID uuid.UUID, assetVersionName string) {
-	lo := models.LicenseOverwrite{
-		LicenseID:      "Apache-2.0",
-		OrganizationId: orgID,
-		ComponentPurl:  "pkg:npm/@xyflow/system@0.0.42",
-		Justification:  "while the specifications place no limits on the magnitude or precision of JSON number literals, the widely used JavaScript implementation stores them as IEEE754 binary64 quantities. For interoperability, applications should avoid transmitting numbers that cannot be represented in this way, for example, 1E400 or 3.141592653589793238462643383279.",
-	}
-	err := db.Create(&lo).Error
-	if err != nil {
-		panic(err)
-	}
+func createComponents(db core.DB, orgID uuid.UUID, assetID uuid.UUID, assetVersionName string) {
 	componentProject := models.ComponentProject{
 		ProjectKey:      "github.com/xyflow/xyflow",
 		StarsCount:      2993,
@@ -78,7 +182,7 @@ func buildDatabase(db core.DB, orgID uuid.UUID, assetID uuid.UUID, assetVersionN
 		License:         "MIT",
 		ScoreCardScore:  utils.Ptr(5.2),
 	}
-	err = db.Create(&componentProject).Error
+	err := db.Create(&componentProject).Error
 	if err != nil {
 		panic(err)
 	}
@@ -106,42 +210,99 @@ func buildDatabase(db core.DB, orgID uuid.UUID, assetID uuid.UUID, assetVersionN
 	if err != nil {
 		panic(err)
 	}
+}
 
-	component_dependency0 := models.ComponentDependency{
-		ComponentPurl:    nil,
-		DependencyPurl:   "pkg:npm/@xyflow/system@0.0.42",
-		AssetID:          assetID,
-		AssetVersionName: assetVersionName,
-		Depth:            0,
-		ScannerIDs:       "SBOM-File-Upload",
+func buildDatabase(db core.DB, orgID uuid.UUID, assetID uuid.UUID, assetVersionName string, mode string) {
+
+	var err error
+	if strings.Contains(mode, "license overwrite") {
+		lo := models.LicenseOverwrite{
+			LicenseID:      "Apache-2.0",
+			OrganizationId: orgID,
+			ComponentPurl:  "pkg:npm/@xyflow/system@0.0.42",
+			Justification:  "while the specifications place no limits on the magnitude or precision of JSON number literals, the widely used JavaScript implementation stores them as IEEE754 binary64 quantities. For interoperability, applications should avoid transmitting numbers that cannot be represented in this way, for example, 1E400 or 3.141592653589793238462643383279.",
+		}
+		err = db.Create(&lo).Error
+		if err != nil {
+			panic(err)
+		}
 	}
-	err = db.Create(&component_dependency0).Error
-	if err != nil {
-		panic(err)
+	if strings.Contains(mode, "components") {
+		componentProject := models.ComponentProject{
+			ProjectKey:      "github.com/xyflow/xyflow",
+			StarsCount:      2993,
+			ForksCount:      282,
+			OpenIssuesCount: 0,
+			License:         "MIT",
+			ScoreCardScore:  utils.Ptr(5.2),
+		}
+		err = db.Create(&componentProject).Error
+		if err != nil {
+			panic(err)
+		}
+		component1 := models.Component{
+			Purl:             "pkg:npm/@xyflow/system@0.0.42",
+			ComponentType:    models.ComponentTypeLibrary,
+			Version:          "0.0.42",
+			License:          utils.Ptr("MIT"),
+			Published:        utils.Ptr(time.Now()),
+			ComponentProject: &componentProject,
+		}
+		err = db.Create(&component1).Error
+		if err != nil {
+			panic(err)
+		}
+		component2 := models.Component{
+			Purl:             "pkg:npm/@xyflow/react@12.3.0",
+			ComponentType:    models.ComponentTypeLibrary,
+			Version:          "12.3.0",
+			License:          utils.Ptr("MIT"),
+			Published:        utils.Ptr(time.Now()),
+			ComponentProject: &componentProject,
+		}
+		err = db.Create(&component2).Error
+		if err != nil {
+			panic(err)
+		}
 	}
 
-	component_dependency1 := models.ComponentDependency{
-		ComponentPurl:    utils.Ptr("pkg:npm/@xyflow/system@0.0.42"),
-		DependencyPurl:   "pkg:npm/@xyflow/react@12.3.0",
-		AssetID:          assetID,
-		AssetVersionName: assetVersionName,
-		Depth:            0,
-		ScannerIDs:       "SBOM-File-Upload",
-	}
-	err = db.Create(&component_dependency1).Error
-	if err != nil {
-		panic(err)
-	}
-	component_dependency2 := models.ComponentDependency{
-		ComponentPurl:    utils.Ptr("pkg:npm/@xyflow/react@12.3.0"),
-		DependencyPurl:   "pkg:npm/@xyflow/system@0.0.42",
-		AssetID:          assetID,
-		AssetVersionName: assetVersionName,
-		Depth:            0,
-		ScannerIDs:       "SBOM-File-Upload",
-	}
-	err = db.Create(&component_dependency2).Error
-	if err != nil {
-		panic(err)
+	if strings.Contains(mode, "dependencies") {
+		component_dependency0 := models.ComponentDependency{
+			ComponentPurl:    nil,
+			DependencyPurl:   "pkg:npm/@xyflow/system@0.0.42",
+			AssetID:          assetID,
+			AssetVersionName: assetVersionName,
+			Depth:            0,
+			ScannerIDs:       "SBOM-File-Upload",
+		}
+		err = db.Create(&component_dependency0).Error
+		if err != nil {
+			panic(err)
+		}
+
+		component_dependency1 := models.ComponentDependency{
+			ComponentPurl:    utils.Ptr("pkg:npm/@xyflow/system@0.0.42"),
+			DependencyPurl:   "pkg:npm/@xyflow/react@12.3.0",
+			AssetID:          assetID,
+			AssetVersionName: assetVersionName,
+			Depth:            0,
+			ScannerIDs:       "SBOM-File-Upload",
+		}
+		err = db.Create(&component_dependency1).Error
+		if err != nil {
+			panic(err)
+		}
+		component_dependency2 := models.ComponentDependency{
+			ComponentPurl:    utils.Ptr("pkg:npm/@xyflow/react@12.3.0"),
+			DependencyPurl:   "pkg:npm/@xyflow/system@0.0.42",
+			AssetID:          assetID,
+			AssetVersionName: assetVersionName,
+			Depth:            0,
+			ScannerIDs:       "SBOM-File-Upload",
+		}
+		err = db.Create(&component_dependency2).Error
+		if err != nil {
+			panic(err)
+		}
 	}
 }
