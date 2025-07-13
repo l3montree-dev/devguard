544e504422269528e251ef6cb253f5690adcad7a
Tim Bastin <bastin.tim@gmail.com>
2025-07-13 13:21:25 +0200

    adds sentry logger to gorm

--- internal/database/database.go
+++ internal/database/database.go
@@ -1,23 +1,73 @@
 package database
 
 import (
+	"context"
 	"database/sql/driver"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"strings"
+	"time"
 
+	"github.com/getsentry/sentry-go"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 )
+
+// create a logger to log any errors to the error tracking
+type sentryLogger struct {
+	defaultLogger logger.Interface
+}
+
+func (s *sentryLogger) LogMode(level logger.LogLevel) logger.Interface {
+	return s
+}
+func (s *sentryLogger) Info(ctx context.Context, msg string, data ...any) {
+	s.sendToSentry(ctx, msg, data...)
+	s.defaultLogger.Info(ctx, msg, data...)
+}
+func (s *sentryLogger) Warn(ctx context.Context, msg string, data ...any) {
+	s.sendToSentry(ctx, msg, data...)
+	s.defaultLogger.Warn(ctx, msg, data...)
+}
+func (s *sentryLogger) Error(ctx context.Context, msg string, data ...any) {
+	s.sendToSentry(ctx, msg, data...)
+	s.defaultLogger.Error(ctx, msg, data...)
+}
+
+func (s *sentryLogger) sendToSentry(ctx context.Context, msg string, data ...any) {
+	if len(data) > 0 {
+		err, ok := data[0].(error)
+		if ok {
+			// check if record not found error
+			if errors.Is(err, gorm.ErrRecordNotFound) {
+				return
+			}
+			sentry.CurrentHub().CaptureException(err)
+		} else {
+			sentry.CurrentHub().CaptureMessage(fmt.Sprintf("%s: %v", msg, data))
+		}
+	} else {
+		sentry.CurrentHub().CaptureMessage(msg)
+	}
+}
+
+func (s *sentryLogger) Trace(ctx context.Context, begin time.Time, fc func() (string, int64), err error) {
+	if err != nil {
+		s.sendToSentry(ctx, "Database error", err)
+	}
+	s.defaultLogger.Trace(ctx, begin, fc, err)
+}
 
 func NewConnection(host, user, password, dbname, port string) (*gorm.DB, error) {
 	// https://github.com/go-gorm/postgres
 	db, err := gorm.Open(postgres.New(postgres.Config{
 		DSN: fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", user, password, host, port, dbname),
 	}), &gorm.Config{
-		Logger: logger.Default,
+		Logger: &sentryLogger{
+			defaultLogger: logger.Default,
+		},
 	})
 
 	if err != nil {
