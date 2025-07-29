5f84797d5c67e45cd1ba6f9da13fa7812f37baaf
Rafi <refaei.shikho@hotmail.com>
2025-07-29 14:47:48 +0200

    Add first-party vulnerability hash migration command and tests
    Signed-off-by: Rafi <refaei.shikho@hotmail.com>

--- cmd/devguard-cli/commands/migrate.go
+++ cmd/devguard-cli/commands/migrate.go
@@ -16,8 +16,8 @@ func NewMigrateCommand() *cobra.Command {
 		Use:   "migrate",
 		Short: "Migrate data",
 	}
-
 	migrate.AddCommand(newDependencyVulnHashMigration())
+	migrate.AddCommand(newFirstPartyVulnHashMigration())
 	return &migrate
 }
 
@@ -86,4 +86,116 @@ func newDependencyVulnHashMigration() *cobra.Command {
 	}
 
 	return &dependencyVulnHashMigration
+}
+
+func newFirstPartyVulnHashMigration() *cobra.Command {
+	firstPartyVulnHashMigration := cobra.Command{
+		Use:   "first-party-vuln-hash",
+		Short: "Will recalculate the firstPartyVuln hashes for all firstPartyVulns",
+		Args:  cobra.ExactArgs(0),
+		Run: func(cmd *cobra.Command, args []string) {
+			core.LoadConfig() // nolint
+			database, err := core.DatabaseFactory()
+			if err != nil {
+				slog.Error("could not connect to database", "err", err)
+				return
+			}
+
+			firstPartyVulnRepository := repositories.NewFirstPartyVulnerabilityRepository(database)
+
+			var firstPartyVulns []models.FirstPartyVuln
+			err = firstPartyVulnRepository.GetDB(nil).Model(&models.FirstPartyVuln{}).Find(&firstPartyVulns).Error
+
+			if err != nil {
+				slog.Error("could not fetch firstPartyVulns", "err", err)
+				return
+			}
+
+			pb := progressbar.Default(int64(len(firstPartyVulns)))
+
+			type firstPartyWithOldHash struct {
+				OldHash        string
+				FirstPartyVuln models.FirstPartyVuln
+			}
+
+			firstPartyVulnMap := make(map[string][]firstPartyWithOldHash)
+
+			for _, firstPartyVuln := range firstPartyVulns {
+				pb.Add(1) //nolint:errcheck
+				oldHash := firstPartyVuln.ID
+				newHash := firstPartyVuln.CalculateHash()
+
+				if oldHash == newHash {
+					// no need to update
+					continue
+				}
+
+				// store the firstPartyVuln in a map to handle duplicates later
+				firstPartyVulnMap[newHash] = append(firstPartyVulnMap[newHash], firstPartyWithOldHash{
+					OldHash:        oldHash,
+					FirstPartyVuln: firstPartyVuln,
+				})
+			}
+			for newHash, firstPartyVulnsWithOldHash := range firstPartyVulnMap {
+				pb.Add(1) //nolint:errcheck
+				if len(firstPartyVulnsWithOldHash) == 1 {
+					// only one firstPartyVuln with this hash, we can update it directly
+					fp := firstPartyVulnsWithOldHash[0]
+					err = firstPartyVulnRepository.GetDB(nil).Model(&models.FirstPartyVuln{}).Where("id = ?", fp.OldHash).UpdateColumn("id", newHash).Error
+					if err != nil {
+						slog.Error("could not update firstPartyVuln", "err", err)
+						continue
+					}
+				} else {
+					// multiple firstPartyVulns with the same hash, we need to merge them
+
+					// create a new firstPartyVuln with the merged snippet contents
+					mergedFirstPartyVuln := firstPartyVulnsWithOldHash[0].FirstPartyVuln
+					mergedSnippetContents := models.SnippetContents{
+						Snippets: []models.SnippetContent{},
+					}
+					for _, fp := range firstPartyVulnsWithOldHash {
+						snippetContents, err := fp.FirstPartyVuln.FromJSONSnippetContents()
+						if err != nil {
+							slog.Error("could not parse snippet contents", "error", err)
+							continue
+						}
+						mergedSnippetContents.Snippets = append(mergedSnippetContents.Snippets, snippetContents.Snippets...)
+					}
+					mergedSnippetJSON, err := mergedSnippetContents.ToJSON()
+					if err != nil {
+						slog.Error("could not convert merged snippet contents to JSON", "error", err)
+						continue
+					}
+					mergedFirstPartyVuln.SnippetContents = mergedSnippetJSON
+
+					// save the merged firstPartyVuln
+					err = firstPartyVulnRepository.GetDB(nil).Model(&models.FirstPartyVuln{}).Save(&mergedFirstPartyVuln).Error
+					if err != nil {
+						slog.Error("could not create merged firstPartyVuln", "err", err)
+						continue
+					}
+					// delete the old firstPartyVulns
+					for _, fp := range firstPartyVulnsWithOldHash {
+						err = firstPartyVulnRepository.GetDB(nil).Model(&models.FirstPartyVuln{}).Where("id = ?", fp.OldHash).Delete(&models.FirstPartyVuln{}, "id = ?", fp.OldHash).Error
+						if err != nil {
+							slog.Error("could not delete old firstPartyVuln", "err", err)
+							continue
+						}
+
+						// update all vuln events to point to the new firstPartyVuln
+						err = firstPartyVulnRepository.GetDB(nil).Model(&models.VulnEvent{}).Where("vuln_id = ?", fp.OldHash).UpdateColumn("vuln_id", newHash).Error
+						if err != nil {
+							slog.Error("could not update vuln events", "err", err)
+							continue
+						}
+
+					}
+				}
+
+			}
+		},
+	}
+
+	return &firstPartyVulnHashMigration
 }
