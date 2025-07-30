98c8ffec8638ec9e3d169eb04f1a56348c60b864
Hubtrick-Git <patrick.rissmann@l3montree.com>
2025-07-30 13:47:04 +0200

    added missing ones

--- cmd/devguard-scanner/commands/intoto/intoto_test.go
+++ cmd/devguard-scanner/commands/intoto/intoto_test.go
@@ -16,22 +16,22 @@ func TestRedactSecret(t *testing.T) {
 	setEmptyEnvVars(t)
 
 	t.Run("RedactSecret", func(t *testing.T) {
-		testStr := "devguard-scanner intoto stop --step=build --products=image-digest.txt --products=image-tag.txt --products=image-tag.txt --token=20b232bf8fde72ebdc2e6e7fe599de4325bdad71b555e2d35e7df8783b4e4e54 --apiURL=https://api.main.devguard.org"
+		testStr := "devguard-scanner intoto stop --step=build --products=image-digest.txt --products=image-tag.txt --products=image-tag.txt --token=20b232bf8fde72ebdc2e6e7fe599de4325bdad71b555e2d35e7df8783b4e4e54 --apiURL=https://api.devguard.org"
 
 		redactedStr := redactSecrets(testStr)
 
-		if redactedStr != "devguard-scanner intoto stop --step=build --products=image-digest.txt --products=image-tag.txt --products=image-tag.txt --REDACTED --apiURL=https://api.main.devguard.org" {
-			t.Errorf("Expected: devguard-scanner intoto stop --step=build --products=image-digest.txt --products=image-tag.txt --products=image-tag.txt --REDACTED --apiURL=https://api.main.devguard.org, got: %s", redactedStr)
+		if redactedStr != "devguard-scanner intoto stop --step=build --products=image-digest.txt --products=image-tag.txt --products=image-tag.txt --REDACTED --apiURL=https://api.devguard.org" {
+			t.Errorf("Expected: devguard-scanner intoto stop --step=build --products=image-digest.txt --products=image-tag.txt --products=image-tag.txt --REDACTED --apiURL=https://api.devguard.org, got: %s", redactedStr)
 		}
 	})
 
 	t.Run("RedactSecret in map", func(t *testing.T) {
 		result := removeSecretsFromMap(map[string]interface{}{
-			"INPUT_ARGS": "devguard-scanner intoto stop --step=build --products=image-digest.txt --products=image-tag.txt --products=image-tag.txt --token=20b232bf8fde72ebdc2e6e7fe599de4325bdad71b555e2d35e7df8783b4e4e54 --apiURL=https://api.main.devguard.org",
+			"INPUT_ARGS": "devguard-scanner intoto stop --step=build --products=image-digest.txt --products=image-tag.txt --products=image-tag.txt --token=20b232bf8fde72ebdc2e6e7fe599de4325bdad71b555e2d35e7df8783b4e4e54 --apiURL=https://api.devguard.org",
 		})
 
-		if result["INPUT_ARGS"] != "devguard-scanner intoto stop --step=build --products=image-digest.txt --products=image-tag.txt --products=image-tag.txt --REDACTED --apiURL=https://api.main.devguard.org" {
-			t.Errorf("Expected: devguard-scanner intoto stop --step=build --products=image-digest.txt --products=image-tag.txt --products=image-tag.txt --REDACTED --apiURL=https://api.main.devguard.org, got: %s", result["INPUT_ARGS"])
+		if result["INPUT_ARGS"] != "devguard-scanner intoto stop --step=build --products=image-digest.txt --products=image-tag.txt --products=image-tag.txt --REDACTED --apiURL=https://api.devguard.org" {
+			t.Errorf("Expected: devguard-scanner intoto stop --step=build --products=image-digest.txt --products=image-tag.txt --products=image-tag.txt --REDACTED --apiURL=https://api.devguard.org, got: %s", result["INPUT_ARGS"])
 		}
 	})
 }
