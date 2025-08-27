d2f52040d00b24888a3e91ce0ab22f4a0c524688
Tim Bastin <38261809+timbastin@users.noreply.github.com>
2025-08-27 15:26:00 +0200

    Update internal/database/repositories/dependency_vuln_repository.go
    Signed-off-by: Tim Bastin <38261809+timbastin@users.noreply.github.com>

--- internal/database/repositories/dependency_vuln_repository.go
+++ internal/database/repositories/dependency_vuln_repository.go
@@ -74,7 +74,7 @@ func (repository *dependencyVulnRepository) GetDependencyVulnsByAssetVersion(tx 
 func (repository *dependencyVulnRepository) GetDependencyVulnsByOtherAssetVersions(tx core.DB, assetVersionName string, assetID uuid.UUID) ([]models.DependencyVuln, error) {
 	var dependencyVulns = []models.DependencyVuln{}
 
-	q := repository.Repository.GetDB(tx).Debug().Preload("Events").Preload("CVE").Preload("CVE.Exploits").Where("dependency_vulns.asset_version_name != ? AND dependency_vulns.asset_id = ?", assetVersionName, assetID)
+	q := repository.Repository.GetDB(tx).Preload("Events").Preload("CVE").Preload("CVE.Exploits").Where("dependency_vulns.asset_version_name != ? AND dependency_vulns.asset_id = ?", assetVersionName, assetID)
 
 	if err := q.Find(&dependencyVulns).Error; err != nil {
 		return nil, err
