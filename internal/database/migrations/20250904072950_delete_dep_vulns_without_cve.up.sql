faf9ce8c18b5b63da379a086733e78026f7b92c8
Tim Bastin <bastin.tim@gmail.com>
2025-09-04 09:31:43 +0200

    deleting vulns and vuln events for missing cves

---
+++ internal/database/migrations/20250904072950_delete_dep_vulns_without_cve.up.sql
@@ -0,0 +1,18 @@
+-- Copyright (C) 2025 l3montree GmbH
+-- 
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU Affero General Public License as
+-- published by the Free Software Foundation, either version 3 of the
+-- License, or (at your option) any later version.
+-- 
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU Affero General Public License for more details.
+-- 
+-- You should have received a copy of the GNU Affero General Public License
+-- along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+DELETE FROM public.dependency_vulns where cve_id IS NULL
+
+DELETE from public.vuln_events WHERE vuln_type = 'dependencyVuln' and NOT EXISTS(SELECT 1 from public.dependency_vulns where id = vuln_id)
