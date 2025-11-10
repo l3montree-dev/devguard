bf46f069fd1fd3fd56104acaf14e37d18ef99e89
Lars <lschwan43@gmail.com>
2025-11-10 18:06:39 +0100

    added role check, since a member have priviledge escalation, never trust the frontend with logic

--- internal/core/vuln/component_occurrence_controller.go
+++ internal/core/vuln/component_occurrence_controller.go
@@ -16,6 +16,7 @@ func NewComponentOccurrenceHTTPController(repository core.ComponentOccurrenceRep
 
 func (controller componentOccurrenceHTTPController) SearchByOrg(ctx core.Context) error {
 	org := core.GetOrg(ctx)
+	role, _ := core.GetRBAC(ctx).GetDomainRole(core.GetSession(ctx).GetUserID())
 
 	pagedResp, err := controller.repository.SearchComponentOccurrencesByOrg(
 		nil,
@@ -25,6 +26,10 @@ func (controller componentOccurrenceHTTPController) SearchByOrg(ctx core.Context
 	)
 	if err != nil {
 		return echo.NewHTTPError(500, "could not search component occurrences").WithInternal(err)
+	}
+
+	if role != core.RoleOwner && role != core.RoleAdmin {
+		return echo.NewHTTPError(403, "owners or admins only")
 	}
 
 	return ctx.JSON(200, pagedResp.Map(func(occurrence models.ComponentOccurrence) any {
