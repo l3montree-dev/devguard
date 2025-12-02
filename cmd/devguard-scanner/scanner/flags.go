a110b8139cc294a4aa7b4766baa9801f1eda496a
rafi <refaei.shikho@hotmail.com>
2025-12-02 14:31:55 +0100

    Add imageSuffix support to generateTag command
    Signed-off-by: rafi <refaei.shikho@hotmail.com>

--- cmd/devguard-scanner/scanner/flags.go
+++ cmd/devguard-scanner/scanner/flags.go
@@ -36,6 +36,7 @@ func AddGenerateTagFlags(cmd *cobra.Command) {
 	cmd.Flags().String("imagePath", "", "Path to the image file (required)")
 	cmd.Flags().String("upstreamVersion", "", "Upstream version of the software")
 	cmd.Flags().StringSlice("architecture", []string{}, "Target architecture(s) for the image (required). Can be specified multiple times or as comma-separated values.")
+	cmd.Flags().String("imageSuffix", "", "Suffix to append to the image tag")
 
 	err := cmd.MarkFlagRequired("imagePath")
 	if err != nil {
