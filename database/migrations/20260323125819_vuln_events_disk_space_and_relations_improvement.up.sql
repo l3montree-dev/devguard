a9808c002b9e2296199a993599025bb1db1b5e1b
Hubtrick-Git <patrick.rissmann@l3montree.com>
2026-03-23 14:26:50 +0100

    added vuln event optimization scripts

---
+++ database/migrations/20260323125819_vuln_events_disk_space_and_relations_improvement.up.sql
@@ -0,0 +1,22 @@
+-- Drop updated_at column to save space since vuln_events are immutable
+ALTER TABLE vuln_events DROP COLUMN IF EXISTS updated_at;
+
+-- Now we want to migrate the existing single vuln_id column to 3 columns referencing the respective vuln id column
+
+-- first create the new rows referencing the id columns
+ALTER TABLE vuln_events
+  ADD COLUMN dependency_vuln_id   UUID REFERENCES dependency_vulns(id)        ON DELETE CASCADE,
+  ADD COLUMN license_risk_id      UUID REFERENCES license_risks(id)           ON DELETE CASCADE,
+  ADD COLUMN first_party_vuln_id  UUID REFERENCES first_party_vulnerabilities(id) ON DELETE CASCADE;
+
+-- then transform and copy the old values into the new columns
+
+UPDATE vuln_events SET dependency_vuln_id  = substring(vuln_id,1,32)::UUID WHERE vuln_type = 'dependencyVulns';
+UPDATE vuln_events SET license_risk_id     = substring(vuln_id,1,32)::UUID WHERE vuln_type = 'licenseRisk';
+UPDATE vuln_events SET first_party_vuln_id = substring(vuln_id,1,32)::UUID WHERE vuln_type = 'firstPartyVuln';
+
+-- lastly drop the old column
+
+ALTER TABLE vuln_events
+  DROP COLUMN vuln_id;
+  -- DROP COLUMN vuln_type; -- maybe still useful
