99128cb47a9965dd0314b15e5e49f06c65dfefba
Tim Bastin <bastin.tim@gmail.com>
2026-03-24 09:38:07 +0100

    running CleanupOrphanedRecordsSQL in a goroutine, adds daemon

--- mocks/mock_InTotoLinkRepository.go
+++ mocks/mock_InTotoLinkRepository.go
@@ -225,6 +225,57 @@ func (_c *InTotoLinkRepository_Begin_Call) RunAndReturn(run func(ctx context.Con
 	return _c
 }
 
+// CleanupOrphanedRecords provides a mock function for the type InTotoLinkRepository
+func (_mock *InTotoLinkRepository) CleanupOrphanedRecords(ctx context.Context) error {
+	ret := _mock.Called(ctx)
+
+	if len(ret) == 0 {
+		panic("no return value specified for CleanupOrphanedRecords")
+	}
+
+	var r0 error
+	if returnFunc, ok := ret.Get(0).(func(context.Context) error); ok {
+		r0 = returnFunc(ctx)
+	} else {
+		r0 = ret.Error(0)
+	}
+	return r0
+}
+
+// InTotoLinkRepository_CleanupOrphanedRecords_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'CleanupOrphanedRecords'
+type InTotoLinkRepository_CleanupOrphanedRecords_Call struct {
+	*mock.Call
+}
+
+// CleanupOrphanedRecords is a helper method to define mock.On call
+//   - ctx context.Context
+func (_e *InTotoLinkRepository_Expecter) CleanupOrphanedRecords(ctx interface{}) *InTotoLinkRepository_CleanupOrphanedRecords_Call {
+	return &InTotoLinkRepository_CleanupOrphanedRecords_Call{Call: _e.mock.On("CleanupOrphanedRecords", ctx)}
+}
+
+func (_c *InTotoLinkRepository_CleanupOrphanedRecords_Call) Run(run func(ctx context.Context)) *InTotoLinkRepository_CleanupOrphanedRecords_Call {
+	_c.Call.Run(func(args mock.Arguments) {
+		var arg0 context.Context
+		if args[0] != nil {
+			arg0 = args[0].(context.Context)
+		}
+		run(
+			arg0,
+		)
+	})
+	return _c
+}
+
+func (_c *InTotoLinkRepository_CleanupOrphanedRecords_Call) Return(err error) *InTotoLinkRepository_CleanupOrphanedRecords_Call {
+	_c.Call.Return(err)
+	return _c
+}
+
+func (_c *InTotoLinkRepository_CleanupOrphanedRecords_Call) RunAndReturn(run func(ctx context.Context) error) *InTotoLinkRepository_CleanupOrphanedRecords_Call {
+	_c.Call.Return(run)
+	return _c
+}
+
 // Create provides a mock function for the type InTotoLinkRepository
 func (_mock *InTotoLinkRepository) Create(ctx context.Context, tx shared.DB, t *models.InTotoLink) error {
 	ret := _mock.Called(ctx, tx, t)
