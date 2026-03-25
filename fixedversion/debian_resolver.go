7228c8d7976612a76699f46869b74a1e9a858029
Tim Bastin <bastin.tim@gmail.com>
2026-03-25 08:39:16 +0100

    alloc maps in constructor

--- fixedversion/debian_resolver.go
+++ fixedversion/debian_resolver.go
@@ -148,18 +148,14 @@ var distroToSuite = map[string]string{
 }
 
 func NewDebianResolver() *DebianResolver {
-	d := DebianResolver{}
+	d := DebianResolver{
+		index:      make(map[distroArch]*packageIndex),
+		timestamps: make(map[distroArch]time.Time),
+	}
 	return &d
 }
 
 func (d *DebianResolver) getPackagesXZ(suite, arch string) (*packageIndex, error) {
-	if d.index == nil {
-		d.index = make(map[distroArch]*packageIndex)
-	}
-	if d.timestamps == nil {
-		d.timestamps = make(map[distroArch]time.Time)
-	}
-
 	key := distroArch{suite, arch}
 	if idx, exists := d.index[key]; exists {
 		// check if older than 12h
