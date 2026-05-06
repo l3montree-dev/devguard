66a46983b7451bf220680aacd10f7e9f37c9fb5c
Hubtrick-Git <patrick.rissmann@l3montree.com>
2026-05-06 15:15:08 +0200

    remove all down migrations

--- database/migrations/20260402110000_add_fixable_distribution_to_artifact_risk_history.down.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE public.artifact_risk_history
-DROP COLUMN IF EXISTS cve_purl_fixable_critical,
-DROP COLUMN IF EXISTS cve_purl_fixable_high,
-DROP COLUMN IF EXISTS cve_purl_fixable_medium,
-DROP COLUMN IF EXISTS cve_purl_fixable_low,
-DROP COLUMN IF EXISTS fixable_critical,
-DROP COLUMN IF EXISTS fixable_high,
-DROP COLUMN IF EXISTS fixable_medium,
-DROP COLUMN IF EXISTS fixable_low;
