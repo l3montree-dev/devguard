c96ab62db23249446a9b0d353d5f8bfcb7dfc24c
rafi <refaei.shikho@hotmail.com>
2026-05-20 10:07:18 +0200

    feat: add cascade delete for github_app_installations and artifact_license_risks
    Signed-off-by: rafi <refaei.shikho@hotmail.com>

---
+++ database/migrations/20260520075840_add_cascade_delete_github_app_installations_and_artifact_license_risks.up.sql
@@ -0,0 +1,13 @@
+ALTER TABLE public.github_app_installations
+    DROP CONSTRAINT IF EXISTS fk_organizations_github_app_installations;
+
+ALTER TABLE public.github_app_installations
+    ADD CONSTRAINT fk_organizations_github_app_installations
+        FOREIGN KEY (org_id) REFERENCES public.organizations(id) ON DELETE CASCADE;
+
+ALTER TABLE public.artifact_license_risks
+    DROP CONSTRAINT IF EXISTS artifact_license_risks_new_license_risk_id_fkey;
+
+ALTER TABLE public.artifact_license_risks
+    ADD CONSTRAINT artifact_license_risks_new_license_risk_id_fkey
+        FOREIGN KEY (new_license_risk_id) REFERENCES public.license_risks(id) ON DELETE CASCADE;
