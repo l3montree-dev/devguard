6d415c6aff63058b7d399464ecf7a401b64313a5
Lars Hermges <lschwan43@gmail.com>
2026-05-26 16:17:32 +0200

    change variable

--- controllers/vulndb_controller.go
+++ controllers/vulndb_controller.go
@@ -229,10 +229,10 @@ func (c VulnDBController) ListIDsByCreationDate(ctx shared.Context) error {
 			return echo.NewHTTPError(400, "invalid limit value").WithInternal(err)
 		}
 
-		sql := `SELECT cve,created_at FROM cves ORDER BY created_at DESC OFFSET ? LIMIT ?;`
+		sql := `SELECT cve,date_published AS created_at FROM cves ORDER BY date_published DESC OFFSET ? LIMIT ?;`
 		err = c.cveRepository.GetDB(ctx.Request().Context(), nil).Raw(sql, offset, limit).Find(&results).Error
 	} else {
-		sql := `SELECT cve,created_at FROM cves ORDER BY created_at DESC OFFSET ?;`
+		sql := `SELECT cve,date_published AS created_at FROM cves ORDER BY date_published DESC OFFSET ?;`
 		err = c.cveRepository.GetDB(ctx.Request().Context(), nil).Raw(sql, offset).Find(&results).Error
 	}
 	if err != nil {
