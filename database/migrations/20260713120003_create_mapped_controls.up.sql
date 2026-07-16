0beec7fe38d7b39bde8563514f9df379a21d53fd
Tim Bastin <bastin.tim@gmail.com>
2026-07-16 13:06:24 +0200

    fixes migration

--- database/migrations/20260713120003_create_mapped_controls.up.sql
+++ database/migrations/20260713120003_create_mapped_controls.up.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS public.mapped_controls (
-    framework_control_id text PRIMARY KEY NOT NULL,
-    related_framework text PRIMARY KEY NOT NULL,
-    related_control_id text PRIMARY KEY NOT NULL
+    framework_control_id text NOT NULL,
+    related_framework text NOT NULL,
+    related_control_id text NOT NULL,
+    PRIMARY KEY (framework_control_id, related_framework, related_control_id)
 );
 
 ALTER TABLE public.mapped_controls DROP CONSTRAINT IF EXISTS fk_mapped_controls_framework_control;
