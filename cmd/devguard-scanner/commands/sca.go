32929b3c0fe297e76c0c056a4d9bc45b695bf403
Tim Bastin <bastin.tim@gmail.com>
2026-07-20 11:01:55 +0200

    adds trivy src for faster scanning

--- cmd/devguard-scanner/commands/sca.go
+++ cmd/devguard-scanner/commands/sca.go
@@ -103,7 +103,7 @@ func generateSBOM(ctx context.Context, pathOrImage string, isImage bool) ([]byte
 		}
 
 		slog.Info("scanning oci image", "image", image)
-		args := []string{"image", image, "--format", "cyclonedx", "--output", sbomFile}
+		args := []string{"image", image, "--image-src", "docker,containerd,podman,remote", "--format", "cyclonedx", "--output", sbomFile}
 		args = append(args, configFileArgs...)
 		args = append(args, config.RuntimeExtraArgs...)
 
