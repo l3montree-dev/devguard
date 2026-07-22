d917578473c4738e0e32ac594160cbc43204bf96
Hubtrick-Git <patrick.rissmann@l3montree.com>
2026-07-22 12:04:16 +0200

    updated mocks

--- mocks/mock_CveRepository.go
+++ mocks/mock_CveRepository.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/l3montree-dev/devguard/database/models"
+	"github.com/l3montree-dev/devguard/dtos"
 	"github.com/l3montree-dev/devguard/shared"
 	mock "github.com/stretchr/testify/mock"
 	"gorm.io/gorm/clause"
@@ -1100,6 +1101,80 @@ func (_c *CveRepository_GetAllCVEsID_Call) Return(strings []string, err error) *
 }
 
 func (_c *CveRepository_GetAllCVEsID_Call) RunAndReturn(run func(ctx context.Context, tx shared.DB) ([]string, error)) *CveRepository_GetAllCVEsID_Call {
+	_c.Call.Return(run)
+	return _c
+}
+
+// GetAllRelatedCVEsForCVE provides a mock function for the type CveRepository
+func (_mock *CveRepository) GetAllRelatedCVEsForCVE(ctx context.Context, tx shared.DB, cveID string) (map[dtos.RelationshipType][]models.CVE, error) {
+	ret := _mock.Called(ctx, tx, cveID)
+
+	if len(ret) == 0 {
+		panic("no return value specified for GetAllRelatedCVEsForCVE")
+	}
+
+	var r0 map[dtos.RelationshipType][]models.CVE
+	var r1 error
+	if returnFunc, ok := ret.Get(0).(func(context.Context, shared.DB, string) (map[dtos.RelationshipType][]models.CVE, error)); ok {
+		return returnFunc(ctx, tx, cveID)
+	}
+	if returnFunc, ok := ret.Get(0).(func(context.Context, shared.DB, string) map[dtos.RelationshipType][]models.CVE); ok {
+		r0 = returnFunc(ctx, tx, cveID)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(map[dtos.RelationshipType][]models.CVE)
+		}
+	}
+	if returnFunc, ok := ret.Get(1).(func(context.Context, shared.DB, string) error); ok {
+		r1 = returnFunc(ctx, tx, cveID)
+	} else {
+		r1 = ret.Error(1)
+	}
+	return r0, r1
+}
+
+// CveRepository_GetAllRelatedCVEsForCVE_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'GetAllRelatedCVEsForCVE'
+type CveRepository_GetAllRelatedCVEsForCVE_Call struct {
+	*mock.Call
+}
+
+// GetAllRelatedCVEsForCVE is a helper method to define mock.On call
+//   - ctx context.Context
+//   - tx shared.DB
+//   - cveID string
+func (_e *CveRepository_Expecter) GetAllRelatedCVEsForCVE(ctx interface{}, tx interface{}, cveID interface{}) *CveRepository_GetAllRelatedCVEsForCVE_Call {
+	return &CveRepository_GetAllRelatedCVEsForCVE_Call{Call: _e.mock.On("GetAllRelatedCVEsForCVE", ctx, tx, cveID)}
+}
+
+func (_c *CveRepository_GetAllRelatedCVEsForCVE_Call) Run(run func(ctx context.Context, tx shared.DB, cveID string)) *CveRepository_GetAllRelatedCVEsForCVE_Call {
+	_c.Call.Run(func(args mock.Arguments) {
+		var arg0 context.Context
+		if args[0] != nil {
+			arg0 = args[0].(context.Context)
+		}
+		var arg1 shared.DB
+		if args[1] != nil {
+			arg1 = args[1].(shared.DB)
+		}
+		var arg2 string
+		if args[2] != nil {
+			arg2 = args[2].(string)
+		}
+		run(
+			arg0,
+			arg1,
+			arg2,
+		)
+	})
+	return _c
+}
+
+func (_c *CveRepository_GetAllRelatedCVEsForCVE_Call) Return(vToCVEs map[dtos.RelationshipType][]models.CVE, err error) *CveRepository_GetAllRelatedCVEsForCVE_Call {
+	_c.Call.Return(vToCVEs, err)
+	return _c
+}
+
+func (_c *CveRepository_GetAllRelatedCVEsForCVE_Call) RunAndReturn(run func(ctx context.Context, tx shared.DB, cveID string) (map[dtos.RelationshipType][]models.CVE, error)) *CveRepository_GetAllRelatedCVEsForCVE_Call {
 	_c.Call.Return(run)
 	return _c
 }
