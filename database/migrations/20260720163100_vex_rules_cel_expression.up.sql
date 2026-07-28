166bf1bb622ccd815d9498d83544c965b56deef1
Tim Bastin <bastin.tim@gmail.com>
2026-07-28 17:17:10 +0200

    adds missing ';' in migration

--- database/migrations/20260720163100_vex_rules_cel_expression.up.sql
+++ database/migrations/20260720163100_vex_rules_cel_expression.up.sql
@@ -4,9 +4,9 @@ ALTER TABLE public.vex_rules ADD COLUMN IF NOT EXISTS cel_expression TEXT NOT NU
 ALTER TABLE public.vex_rules ADD COLUMN IF NOT EXISTS title TEXT NOT NULL DEFAULT '';
 
 
-UPDATE public.vex_rules SET cel_expression = format('vuln.cveId == "%s" && matchesPattern(vuln, %s)', cve_id::text, path_pattern::text)
+UPDATE public.vex_rules SET cel_expression = format('vuln.cveId == "%s" && matchesPattern(vuln, %s)', cve_id::text, path_pattern::text);
 
-UPDATE public.vex_rules SET title = format('VEX rule for %s', cve_id::text)
+UPDATE public.vex_rules SET title = format('VEX rule for %s', cve_id::text);
 
 ALTER TABLE public.vex_rules DROP COLUMN path_pattern;
 
