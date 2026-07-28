22466f023e248167e66dd3f83f0564249553d67b
Tim Bastin <bastin.tim@gmail.com>
2026-07-28 12:38:33 +0200

    dropping asset_version_name column, fixes cel vex rules migration

--- database/migrations/20260720163100_vex_rules_cel_expression.up.sql
+++ database/migrations/20260720163100_vex_rules_cel_expression.up.sql
@@ -3,22 +3,13 @@ ALTER TABLE public.vex_rules ADD COLUMN IF NOT EXISTS cel_expression TEXT NOT NU
 
 ALTER TABLE public.vex_rules ADD COLUMN IF NOT EXISTS title TEXT NOT NULL DEFAULT '';
 
-ALTER TABLE public.vex_rules DROP COLUMN IF EXISTS cve_id;
 
-DO $$
-BEGIN
-    IF EXISTS (
-        SELECT 1
-        FROM information_schema.columns
-        WHERE table_schema = 'public'
-          AND table_name = 'vex_rules'
-          AND column_name = 'path_pattern'
-    ) THEN
-        UPDATE public.vex_rules
-        SET cel_expression = format('matchesPattern(vuln, %s)', path_pattern::text)
-        WHERE cel_expression = '';
+UPDATE public.vex_rules SET cel_expression = format('vuln.cveId == "%s" && matchesPattern(vuln, %s)', cve_id::text, path_pattern::text)
 
-        ALTER TABLE public.vex_rules
-        DROP COLUMN path_pattern;
-    END IF;
-END $$;
+UPDATE public.vex_rules SET title = format('VEX rule for %s', cve_id::text)
+
+ALTER TABLE public.vex_rules DROP COLUMN path_pattern;
+
+ALTER TABLE public.vex_rules DROP COLUMN cve_id;
+
+ALTER TABLE public.vex_rules DROP COLUMN asset_version_name;
