f82e9399f9bb560bb002a7c4a03ffd9947696644
Julian Kepka <julian.kepka@l3montree.com>
2026-08-20 09:09:12 +0200

    fix: adjusted migration

--- database/migrations/20260819075101_adjustAssetTable.up.sql
+++ database/migrations/20260819075101_adjustAssetTable.up.sql
@@ -3,4 +3,8 @@ ALTER TABLE IF EXISTS public.assets
     ADD COLUMN modified_attack_complexity text DEFAULT 'X'::text NOT NULL,
     ADD COLUMN modified_privileges_required text DEFAULT 'X'::text NOT NULL,
     ADD COLUMN modified_scope text DEFAULT 'X'::text NOT NULL,
+    ADD COLUMN modified_user_interaction text DEFAULT 'X'::text NOT NULL,
+    ADD COLUMN modified_confidentiality text DEFAULT 'X'::text NOT NULL,
+    ADD COLUMN modified_integrity text DEFAULT 'X'::text NOT NULL,
+    ADD COLUMN modified_availability text DEFAULT 'X'::text NOT NULL,
     DROP COLUMN reachable_from_internet;
