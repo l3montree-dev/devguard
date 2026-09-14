e53b7ec75ca4279d28552e37501cc77ac7174a06
rafi <refaei.shikho@hotmail.com>
2026-09-14 16:30:03 +0200

    fix: include rpm packages when prepending epoch to purl version
    Signed-off-by: rafi <refaei.shikho@hotmail.com>

--- normalize/purl.go
+++ normalize/purl.go
@@ -44,9 +44,9 @@ func ParsePurlForMatching(purl packageurl.PackageURL) *PurlMatchContext {
 		versionInterpretation = EcosystemSpecificVersion
 		normalizedVersion = purl.Version
 
-		// For Debian packages, prepend epoch from qualifier if present
+		// For Debian packages and RPM packages, prepend epoch from qualifier if present
 		// e.g., pkg:deb/debian/git@2.47.3-0+deb13u1?epoch=1 -> "1:2.47.3-0+deb13u1"
-		if purl.Type == "deb" {
+		if purl.Type == "deb" || purl.Type == "rpm" {
 			if epoch := qualifier.Map()["epoch"]; epoch != "" {
 				normalizedVersion = epoch + ":" + normalizedVersion
 			}
